93d9ee3ce250f09540e4e164ecf40f5b1cc7547f
Burak <none@none.com>
2011-09-02 02:22:13 +0300

    ..

--- app-math-tr/collat.sh
+++ app-math-tr/collat.sh
@@ -1,7 +1,7 @@
 pdftk */cover.pdf */model.pdf */karesel.pdf */euler.pdf */taylor.pdf \
 */logaritma.pdf */complexity.pdf */probsolve.pdf */id3.pdf \
 */turev.pdf */totaldiff.pdf */eigseg.pdf */rayleigh.pdf \
-*/dagilimlar.pdf */buyuk.pdf */cebisev.pdf */ml-tr.pdf */naive.pdf \
+*/moment.pdf */dagilimlar.pdf */buyuk.pdf */cebisev.pdf */ml-tr.pdf */naive.pdf \
 */coal.pdf */simplex.pdf */qp.pdf */svm.pdf */fem.pdf */fourier.pdf \
 */heat.pdf */curvature.pdf */level.pdf */varcalc.pdf */filter.pdf \
 */phd.pdf output ~/Dropbox/Public/skfiles/app-math-tr.pdf 
