376b86314b4771bab7a31717179b7df70deb8c5d
Burak <none@none.com>
2011-09-29 15:02:37 +0300

    ..

--- app-math-tr/taylor/build.py
+++ app-math-tr/taylor/build.py
@@ -4,6 +4,3 @@ if len(sys.argv) == 1 or sys.argv[1] == 'tex':
     os.system("pdflatex taylor.tex")
     os.system("evince taylor.pdf")
     exit()
-
-if sys.argv[1] == 'zip':
-    os.system("zip ~/Dropbox/Public/skfiles/taylor.zip taylor.tex taylor.pdf build.py")
