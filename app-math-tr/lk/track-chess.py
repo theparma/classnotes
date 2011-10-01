828aec9184e1a1c5bdc36e52e2f65c21899496db
Burak <none@none.com>
2011-10-01 14:33:01 +0300

    ..

--- app-math-tr/lk/track-chess.py
+++ app-math-tr/lk/track-chess.py
@@ -8,18 +8,24 @@ import Image
 import lk
 import math
 
-prev = None
-track_x = 100
-track_y = 30
-win = 5
-__scale__ = 3
 __test__ = "/home/burak/Dropbox/Public/skfiles/campy/chessb-right.avi"
+prev = None
+track_x = 150
+track_y = 50
+__scale__ = 3
+#track_x = 220
+#track_y = 70
+#__scale__ = 2
+#track_x = 110
+#track_y = 40
+#__scale__ = 4
+win = 5
+frame_no = 0
 
 if __name__ == '__main__':
     cvNamedWindow("Example2", CV_WINDOW_AUTOSIZE)
     capture = cvCreateFileCapture(__test__)
     loop = True
-    __scale__ = 4
     while(loop):
         frame = cvQueryFrame(capture)
         if (frame == None): break
@@ -29,20 +35,22 @@ if __name__ == '__main__':
         ipl = ipl.resize((int(frame.width/__scale__),
                           int(frame.height/__scale__)), 
                          Image.ANTIALIAS)
-        curr = np.array(ipl)
         
-        if prev != None: 
-            u, v = lk.lk(prev, curr, track_x, track_y, win)              
-            print u, v
-            track_x += u
-            track_y += v
-        print track_x, track_y
-        prev = curr            
-
+        if frame_no > 25:
+            curr = np.array(ipl)        
+            if prev != None: 
+                u, v = lk.lk(prev, curr, track_x, track_y, win)              
+                print u, v
+                track_x += u
+                track_y += v
+            prev = curr            
+        
         ipl2 = PIL2Ipl(ipl)
         cvCircle(ipl2, (int(track_x), int(track_y)), 
-                 2, CV_RGB(255,255,255), 0, CV_AA, 0 )              
+                 2, CV_RGB(255,255,255), 0, CV_AA, 0 )   
         cvShowImage("Example2", ipl2)
+        
+        frame_no += 1
         
         char = cvWaitKey(33)
         if (char != -1):
