91d7d60d1e6d3d72b49695bddb5d69c24ea1e1c1
Burak <none@none.com>
2011-10-01 14:25:51 +0300

    ..

--- app-math-tr/lk/track-chess.py
+++ app-math-tr/lk/track-chess.py
@@ -1,34 +1,36 @@
 from opencv.cv import *
 from opencv.highgui import *
 from opencv.adaptors import Ipl2NumPy
+from opencv.adaptors import Ipl2PIL
+from opencv.adaptors import PIL2Ipl
 import numpy as np
+import Image
 import lk
 import math
 
 prev = None
-track_x = 420
-track_y = 100
-win = 10
-__scale__ = 4
+track_x = 100
+track_y = 30
+win = 5
+__scale__ = 3
+__test__ = "/home/burak/Dropbox/Public/skfiles/campy/chessb-right.avi"
 
 if __name__ == '__main__':
-    file = "/home/burak/Dropbox/Public/skfiles/campy/chessb-right.avi"
     cvNamedWindow("Example2", CV_WINDOW_AUTOSIZE)
-    capture = cvCreateFileCapture(file)
+    capture = cvCreateFileCapture(__test__)
     loop = True
     __scale__ = 4
     while(loop):
         frame = cvQueryFrame(capture)
         if (frame == None): break
         gray = cvCreateImage ((frame.width, frame.height), 8, 1)   
-        cvCvtColor( frame, gray, CV_BGR2GRAY )        
-        curr = Ipl2NumPy(gray)
-        curr = np.resize(gray,(int(frame.width/__scale__), 
-                               int(frame.height/__scale__)))
+        cvCvtColor( frame, gray, CV_BGR2GRAY )                
+        ipl = Ipl2PIL(gray)
+        ipl = ipl.resize((int(frame.width/__scale__),
+                          int(frame.height/__scale__)), 
+                         Image.ANTIALIAS)
+        curr = np.array(ipl)
         
-        cvCircle(frame, (int(track_x), int(track_y)), 
-                 4, CV_RGB(0,255,0), 0, CV_AA, 0 )
-                 
         if prev != None: 
             u, v = lk.lk(prev, curr, track_x, track_y, win)              
             print u, v
@@ -36,8 +38,11 @@ if __name__ == '__main__':
             track_y += v
         print track_x, track_y
         prev = curr            
-                
-        cvShowImage("Example2", frame)
+
+        ipl2 = PIL2Ipl(ipl)
+        cvCircle(ipl2, (int(track_x), int(track_y)), 
+                 2, CV_RGB(255,255,255), 0, CV_AA, 0 )              
+        cvShowImage("Example2", ipl2)
         
         char = cvWaitKey(33)
         if (char != -1):
