2bdba5198c1fb7090bf3c6eb88270b2c90dc367d
Burak <none@none.com>
2011-10-19 16:36:25 +0300

    ..

--- calc_multi/calc_multi_08/ellipsoid.py
+++ calc_multi/calc_multi_08/ellipsoid.py
@@ -7,24 +7,21 @@ import numpy as np
 fig = plt.figure(figsize=plt.figaspect(1))  # Square figure
 ax = fig.add_subplot(111, projection='3d')
 
-coefs = (1, 4, 10)  # Coefficients in a0/c x**2 + a1/c y**2 + a2/c z**2 = 1 
-# Radii corresponding to the coefficients:
+# Katsayilar a0/c x**2 + a1/c y**2 + a2/c z**2 = 1 
+coefs = (1, 4, 10)  
+
+# Katsayilara tekabul eden caplar
 rx, ry, rz = [1/np.sqrt(coef) for coef in coefs]
 
-# Set of all spherical angles:
 u = np.linspace(0, 2 * np.pi, 100)
 v = np.linspace(0, np.pi, 100)
 
-# Cartesian coordinates that correspond to the spherical angles:
-# (this is the equation of an ellipsoid):
 x = rx * np.outer(np.cos(u), np.sin(v))
 y = ry * np.outer(np.sin(u), np.sin(v))
 z = rz * np.outer(np.ones_like(u), np.cos(v))
 
-# Plot:
 ax.plot_surface(x, y, z,  rstride=4, cstride=4, color='b')
 
-# Adjustment of the axes, so that they all have the same span:
 max_radius = max(rx, ry, rz)
 ax.set_xlim(-max_radius, max_radius)
 ax.set_ylim(-max_radius, max_radius)
