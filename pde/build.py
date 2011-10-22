ca68b2b77923ea86c1333bdc37c72da3c60956d9
Burak <none@none.com>
2011-10-22 16:03:44 +0300

    ..

--- pde/build.py
+++ pde/build.py
@@ -1,7 +1,7 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdftk pde*/*.pdf output ~/Dropbox/Public/skfiles/multivar_calculus.pdf")
+    os.system("pdftk pde*/*.pdf output ~/Dropbox/Public/skfiles/pde.pdf")
     exit()
     
     
