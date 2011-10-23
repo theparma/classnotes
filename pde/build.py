5785f88121eaec0eb104dc1b760964962742a9b0
Burak <none@none.com>
2011-10-23 18:22:56 +0300

    ..

--- pde/build.py
+++ pde/build.py
@@ -1,7 +1,7 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdftk pde*/*.pdf output ~/Dropbox/Public/skfiles/pde.pdf")
+    os.system("pdftk pde*/pde*.pdf output ~/Dropbox/Public/skfiles/pde.pdf")
     exit()
     
     
