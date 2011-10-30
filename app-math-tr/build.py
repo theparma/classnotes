0d6c5c37a7009a711c873e8636513840d0b14121
Burak <none@none.com>
2011-10-30 17:29:52 +0200

    ..

--- app-math-tr/build.py
+++ app-math-tr/build.py
@@ -6,7 +6,7 @@ if len(sys.argv) == 1 or sys.argv[1] == 'tex':
 */logaritma.pdf */complexity.pdf */probsolve.pdf */id3.pdf \
 */turev.pdf */totaldiff.pdf */eigseg.pdf */rayleigh.pdf \
 */moment.pdf */dagilimlar.pdf */buyuk.pdf */cebisev.pdf \
-*/ml-tr.pdf */naive.pdf */coal.pdf */simplex.pdf */qp.pdf \
+*/ml-tr.pdf */naive.pdf */mcmc.pdf */coal.pdf */simplex.pdf */qp.pdf \
 */svm.pdf */fem.pdf */fourier.pdf */heat.pdf */curvature.pdf \
 */level.pdf */lk.pdf */varcalc.pdf */filter.pdf */phd.pdf \
 output ~/Dropbox/Public/skfiles/app-math-tr.pdf "
