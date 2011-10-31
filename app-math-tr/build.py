ed1bcbe58f954da1c3bf2702163574e4efd6a8e3
Burak <none@none.com>
2011-10-31 11:39:21 +0200

    ..

--- app-math-tr/build.py
+++ app-math-tr/build.py
@@ -5,7 +5,7 @@ if len(sys.argv) == 1 or sys.argv[1] == 'tex':
 */fundamental.pdf */integral-mult.pdf */euler.pdf */taylor.pdf */taylor2d.pdf \
 */logaritma.pdf */complexity.pdf */probsolve.pdf */id3.pdf \
 */turev.pdf */totaldiff.pdf */eigseg.pdf */rayleigh.pdf \
-*/moment.pdf */dagilimlar.pdf */buyuk.pdf */cebisev.pdf \
+exp/exp.pdf */moment.pdf */dagilimlar.pdf */buyuk.pdf */cebisev.pdf \
 */ml-tr.pdf */naive.pdf */mcmc.pdf */coal.pdf */simplex.pdf */qp.pdf \
 */svm.pdf */fem.pdf */fourier.pdf */heat.pdf */curvature.pdf \
 */level.pdf */lk.pdf */varcalc.pdf */filter.pdf */phd.pdf \
