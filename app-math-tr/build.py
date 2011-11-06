74873c8f8a849021e65c1ae83b3e7da30c29af4f
Burak <none@none.com>
2011-11-07 01:20:13 +0200

    ..

--- app-math-tr/build.py
+++ app-math-tr/build.py
@@ -3,7 +3,7 @@ import os, sys
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
     cmd = "pdftk */cover.pdf */model.pdf */karesel.pdf */invtrig.pdf \
 */fundamental.pdf */integral-mult.pdf */euler.pdf */taylor.pdf */taylor2d.pdf \
-*/logaritma.pdf */complexity.pdf */probsolve.pdf */id3.pdf \
+eig/eig.pdf */logaritma.pdf */complexity.pdf */probsolve.pdf */id3.pdf \
 */turev.pdf */totaldiff.pdf */eigseg.pdf */rayleigh.pdf \
 exp/exp.pdf */moment.pdf */dagilimlar.pdf */buyuk.pdf */cebisev.pdf \
 */ml-tr.pdf */naive.pdf */mcmc.pdf */coal.pdf */simplex.pdf */qp.pdf \
