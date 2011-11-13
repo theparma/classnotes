7b38b45527249d324ed2ae664ae616cafead5e23
Burak <none@none.com>
2011-11-13 15:36:49 +0200

    ..

--- calc_multi/build.py
+++ calc_multi/build.py
@@ -1,7 +1,13 @@
-import os, sys
+import os, sys, glob
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
     os.system("pdftk calc_multi*/*.pdf output ~/Dropbox/Public/skfiles/multivar_calculus.pdf")
     exit()
     
+if sys.argv[1] == 'all':
+    for a in sorted(glob.glob("calc*")):
+        os.chdir(a)
+        os.system("pdflatex calc_mul*.tex")    
+        os.chdir("..")
+        
     
