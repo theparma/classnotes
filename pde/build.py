d2200d0e8054d8ffd5b39cda060a470a749a89c5
Burak <none@none.com>
2011-11-14 20:29:26 +0200

    ..

--- pde/build.py
+++ pde/build.py
@@ -1,7 +1,13 @@
-import os, sys
+import os, sys, glob
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdftk pde*/pde*.pdf output ~/Dropbox/Public/skfiles/pde.pdf")
+    os.system("pdftk pde*/*.pdf output ~/Dropbox/Public/skfiles/pde.pdf")
     exit()
+    
+if sys.argv[1] == 'all':
+    for a in sorted(glob.glob("pde*")):
+        os.chdir(a)
+        os.system("pdflatex pde*.tex")    
+        os.chdir("..")
     
     
