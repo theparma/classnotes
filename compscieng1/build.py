9929fe46fdb350b8f1b7e2fc237fc8d83ceec6a4
Burak <none@none.com>
2011-11-14 17:53:36 +0200

    ..

---
+++ compscieng1/build.py
@@ -0,0 +1,13 @@
+import os, sys, glob
+
+if len(sys.argv) == 1 or sys.argv[1] == 'tex':
+    os.system("pdftk compscieng*/*.pdf output ~/Dropbox/Public/skfiles/compscieng1.pdf")
+    exit()
+    
+if sys.argv[1] == 'all':
+    for a in sorted(glob.glob("compscieng*")):
+        os.chdir(a)
+        os.system("pdflatex compscieng*.tex")    
+        os.chdir("..")
+        
+    
