579e421c98b14808a1266f1d1b56452f849c4202
Burak <none@none.com>
2011-11-14 17:47:30 +0200

    ..

--- ode/build.py
+++ ode/build.py
@@ -1,7 +1,13 @@
-import os, sys
+import os, sys, glob
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdftk */ode_mattuck*.pdf output ~/Dropbox/Public/skfiles/ode_mattuck.pdf")
+    os.system("pdftk ode_mattuck*/*.pdf output ~/Dropbox/Public/skfiles/ode_mattuck.pdf")
     exit()
+    
+if sys.argv[1] == 'all':
+    for a in sorted(glob.glob("ode*")):
+        os.chdir(a)
+        os.system("pdflatex ode_mattuck*.tex")    
+        os.chdir("..")
     
     
