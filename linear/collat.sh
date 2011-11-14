5ebb645ec2583d2570da3d10c40996c04e8002c9
Burak <none@none.com>
2011-11-14 20:25:36 +0200

    ..

--- linear/collat.sh
+++
@@ -1,1 +0,0 @@
-pdftk */*.pdf output ~/Dropbox/Public/skfiles/linear_strang.pdf
--- linear/linear_23/build.py
+++ linear/linear_23/build.py
@@ -5,8 +5,3 @@ if len(sys.argv) == 1 or sys.argv[1] == 'tex':
     os.system("evince linear_*.pdf")
     exit()
     
-if sys.argv[1] == 'zip':    
-    cmd = "zip ~/Dropbox/Public/skfiles/linear/linear_%d.zip linear_%d.tex linear_%d.pdf *.png *.py" % \
-        (int(sys.argv[2]), int(sys.argv[2]), int(sys.argv[2]))
-    os.system(cmd)
-    
