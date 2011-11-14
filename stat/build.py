647685f0596c987e29e9d03b23adfd37e6ef2385
Burak <none@none.com>
2011-11-14 18:20:55 +0200

    ..

--- stat/build.py
+++ stat/build.py
@@ -1,7 +1,13 @@
-import os, sys
+import os, sys, glob
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdftk stat*/stat*.pdf output ~/Dropbox/Public/skfiles/stat.pdf")
+    os.system("pdftk stat*/*.pdf output ~/Dropbox/Public/skfiles/stat.pdf")
     exit()
+    
+if sys.argv[1] == 'all':
+    for a in sorted(glob.glob("stat*")):
+        os.chdir(a)
+        os.system("pdflatex stat*.tex")    
+        os.chdir("..")
     
     
