c415d84e69f5bd9690d7ead06b02009c48bc2a94
Burak <none@none.com>
2011-11-16 15:09:22 +0200

    ..

--- app-math-tr/model/fit.py
+++ app-math-tr/model/fit.py
@@ -12,8 +12,8 @@ def resid(p, y, t):
 if __name__ == '__main__':
     #t, x1 = np.loadtxt('us.txt', unpack=True)
     t, x1 = np.loadtxt('world.txt', unpack=True)
+    print len(t)
     t = np.linspace(0,1,len(t))
-    print t
     
     A0,k0,K0 = 1, 1, 1
     [A,k,K], flag  = optimize.leastsq(resid, [A0,k0,K0], args=(x1, t))
