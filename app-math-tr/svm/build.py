8edc42d6247d12b41e7c7d5dcd55423a9b467403
Burak <none@none.com>
2011-11-19 23:50:36 +0200

    ..

--- app-math-tr/svm/build.py
+++ app-math-tr/svm/build.py
@@ -5,7 +5,8 @@ if len(sys.argv) == 1 or sys.argv[1] == 'tex':
     os.system("evince svm.pdf")
     exit()
     
-if sys.argv[1] == 'zip':    
-    cmd = "zip ~/Dropbox/Public/skfiles/svm-tr.zip build.py *.tex *.pdf *.png *.py"
-    os.system(cmd)
+if sys.argv[1] == 'en':
+    os.system("pdflatex svm-en.tex")
+    os.system("evince svm-en.pdf")
+    exit()
     
