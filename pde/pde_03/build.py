03e162840a71efc51e39ad85f4112ee649fd557c
Burak <none@none.com>
2011-11-28 18:55:39 +0200

    ..

--- pde/pde_03/build.py
+++ pde/pde_03/build.py
@@ -1,7 +1,18 @@
-import os, sys
+import os, sys, re
+
+def run_command(command):
+    result = []
+    f = os.popen(command, "r")
+    sys.stdout.flush()
+    for l in f.xreadlines():
+        result.append(l)
+    return result
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex pde*.tex")
-    os.system("evince pde*.pdf")
-    exit()
-    
+    s = run_command("pdflatex pde*.tex")
+    s = run_command("pdflatex pde*.tex")
+    print s
+    for ss in s: 
+        f = re.search('There were multiply-defined labels', ss)
+        if f: exit()
+    os.system("evince pde*.pdf")    
