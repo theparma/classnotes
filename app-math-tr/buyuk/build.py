036c2d5f312c0ca572dbd8e973965274e2b23adb
Burak <none@none.com>
2011-12-26 01:00:53 +0200

    ..

--- app-math-tr/buyuk/build.py
+++ app-math-tr/buyuk/build.py
@@ -4,7 +4,3 @@ if len(sys.argv) == 1 or sys.argv[1] == 'tex':
     os.system("pdflatex buyuk.tex")
     os.system("evince  buyuk.pdf")
     exit()
-
-if sys.argv[1] == 'zip':    
-    cmd = "zip ~/Dropbox/Public/skfiles/buyuk.zip buyuk.pdf buyuk.tex build.py"
-    os.system(cmd)
