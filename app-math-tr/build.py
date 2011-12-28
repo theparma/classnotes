6e25a4418db3a225f50b27a245eb535f78c7164d
Burak <none@none.com>
2011-12-29 01:48:41 +0200

    ..

--- app-math-tr/build.py
+++ app-math-tr/build.py
@@ -8,7 +8,7 @@ pfractions/pfractions.pdf ratio/ratio.pdf poldiv/poldiv.pdf */fundamental.pdf  \
  */id3.pdf */turev.pdf */totaldiff.pdf */eigseg.pdf */rayleigh.pdf \
 exp/exp.pdf */moment.pdf */dagilimlar.pdf */buyuk.pdf */cebisev.pdf \
 */ml-tr.pdf */naive.pdf */mcmc.pdf */coal.pdf */simplex.pdf */qp.pdf \
-*/svm.pdf */fem.pdf */fourier.pdf pde*/pde_01.pdf */heat.pdf */curvature.pdf \
+*/svm.pdf */fem.pdf */fourier.pdf pde-wave-deriv/pde_01.pdf */heat.pdf */curvature.pdf \
 */level.pdf */lk.pdf */varcalc.pdf */filter.pdf */phd.pdf \
 output ~/Dropbox/Public/skfiles/app-math-tr.pdf "
     os.system(cmd)
