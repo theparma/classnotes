a0676831c8faa7021fd884a521402889257d7c4c
Burak <none@none.com>
2012-01-10 15:17:42 +0200

    ..

---
+++ func_analysis/build.py
@@ -0,0 +1,12 @@
+import os, sys, glob
+
+if len(sys.argv) == 1 or sys.argv[1] == 'tex':
+    os.system("pdftk func*/*.pdf output ~/Dropbox/Public/skfiles/functional_analysis.pdf")
+    exit()
+    
+if sys.argv[1] == 'all':
+    for a in sorted(glob.glob("func*")):
+        os.chdir(a)
+        os.system("pdflatex func*.tex")    
+        os.chdir("..")
+        
