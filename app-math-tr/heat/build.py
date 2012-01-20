fcd91969be080d88adf72e5e55f820e9e4b3e56a
Burak <none@none.com>
2012-01-20 14:26:36 +0200

    ..

--- app-math-tr/heat/build.py
+++ app-math-tr/heat/build.py
@@ -5,8 +5,5 @@ if len(sys.argv) == 1 or sys.argv[1] == 'tex':
     os.system("evince heat.pdf")
     exit()
 
-if sys.argv[1] == 'zip':    
-    cmd = "zip ~/Dropbox/Public/skfiles/heat-fdiff.zip *.py *.pdf *.tex"
-    os.system(cmd)
     
     
