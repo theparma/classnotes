0466ea92f4e948898eec7c0267104180ffe23431
Burak <none@none.com>
2012-01-22 14:36:14 +0200

    ..

--- build.py
+++ build.py
@@ -2,3 +2,5 @@ import os, sys
 
 if sys.argv[1] == 'zip':
     os.system("zip ~/Dropbox/classnotes.zip -y -r * --exclude=.git/* --exclude=*.pdf --exclude=*.PDF --exclude=*.aux --exclude=*.log --exclude=*.pyc  --exclude=*/*/_region* . ")
+if sys.argv[1] == 'clean':
+    os.system("find . -name _region_* | xargs rm -rf ")
