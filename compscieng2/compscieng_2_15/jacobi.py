27651d56f3574a2a4c68144ab8a8b30d16f36b29
Burak <none@none.com>
2012-02-13 14:18:29 +0200

    ..

--- compscieng2/compscieng_2_15/jacobi.py
+++ compscieng2/compscieng_2_15/jacobi.py
@@ -1,13 +1,10 @@
 import numpy as np
 import scipy.linalg as lin
 
-#np.linalg.lstsq(B,b)
-
 A = np.array([[6.,1.,1.],
               [1.,7.,1.],
               [1.,1.,8.]])
 b = [1.,1.,1.]
-print A
 
 xreal = lin.solve(A, b); print "solution", xreal
 
--- compscieng2/compscieng_2_15/jacobi2.py
+++ compscieng2/compscieng_2_15/jacobi2.py
@@ -1,13 +1,11 @@
 import numpy as np
 import scipy.linalg as lin
 
-#np.linalg.lstsq(B,b)
-
 A = np.array([[6.,1.,1.],
               [1.,7.,1.],
               [1.,1.,8.]])
+
 b = [1.,1.,1.]
-print A
 
 xreal = lin.solve(A, b); print "solution", xreal
 
@@ -15,8 +13,6 @@ P = np.diag(np.diag(A)); print "P",P
 x = np.zeros(A.shape[0]); print x
 J = lin.solve(P,P-A)
 c = lin.solve(P,b)
-print "J",J
-print "c",c
 for i in range(10):
     x = np.dot(J,x) + c
     print x
