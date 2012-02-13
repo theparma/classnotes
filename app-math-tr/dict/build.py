a37b41cc4e3cb3d8ae91520b0e11be20ec99d964
Burak <none@none.com>
2012-02-13 10:14:34 +0200

    ..

--- app-math-tr/dict/build.py
+++ app-math-tr/dict/build.py
@@ -1,16 +1,20 @@
 import os, sys
 
-if len(sys.argv) == 1 or sys.argv[1] == 'tex1':
+if len(sys.argv) == 1:
+    print "choose dict number"
+    exit()
+
+if sys.argv[1] == 'tex1':
     os.system("pdflatex dict1.tex")
     os.system("evince dict1.pdf")
     exit()
 
-if len(sys.argv) == 1 or sys.argv[1] == 'tex2':
+if sys.argv[1] == 'tex2':
     os.system("pdflatex dict2.tex")
     os.system("evince dict2.pdf")
     exit()
 
-if len(sys.argv) == 1 or sys.argv[1] == 'tex3':
+if sys.argv[1] == 'tex3':
     os.system("pdflatex dict3.tex")
     os.system("evince dict3.pdf")
     exit()
