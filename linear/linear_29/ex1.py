bb3de49a1be70b1bb652c40974ff571a3ed6c3c6
Burak <none@none.com>
2012-02-29 13:06:28 +0200

    ..

---
+++ linear/linear_29/ex1.py
@@ -0,0 +1,36 @@
+import scipy.linalg as lin
+import numpy as np
+
+a = np.array([[4,4],[-3,3]])
+print np.dot(a.T,a)
+
+print '\nEig of A.T*A\n'
+a = np.array([[25,7],[7,25]])
+w,vl =  lin.eig(a)
+print w
+print vl
+
+print '\nEig of A*A.T\n'
+a = np.array([[32,0],[0,18]])
+w,vl =  lin.eig(a)
+print w
+print vl
+
+a1 = np.array([[1,0],[0,1]])
+a2 = np.array([[np.sqrt(32),0],[0,np.sqrt(18)]])
+a3 = np.array([[1./np.sqrt(2),1./np.sqrt(2)],
+               [1./np.sqrt(2),-1./np.sqrt(2)]])
+
+print np.dot(a1,np.dot(a2,a3))
+
+exit()
+
+print '\nReal SVD\n'
+
+a = np.array([[4,-3],[4,3]])
+U,s,Vh =  lin.svd(a)
+
+print U
+print s
+print Vh
+
