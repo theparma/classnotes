c3836521c404336ace47b753618b5cf983c96a2c
Burak Bayramli <burak@lieferheld.de>
2012-03-26 16:09:37 +0200

    -

---
+++ data/data_01/preskde.py
@@ -0,0 +1,12 @@
+# Calculating kernel density estimates
+from numpy import *
+
+# z: position, w: bandwidth, xv: vector of points
+def kde( z, w, xv ):
+    print xv.shape
+    return sum( exp(-0.5*((z-xv)/w)**2)/sqrt(2*pi*w**2) )
+
+d = loadtxt("pres.txt",  usecols = (1,))
+w = 2.5
+for x in linspace( min(d)-w, max(d)+w, 1000 ):
+    print x, kde( x, w, d )
