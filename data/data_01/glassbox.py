15df6a3d32a74cf894489062a49c9a620f988391
Burak Bayramli <burak@lieferheld.de>
2012-03-26 12:58:15 +0200

    -

---
+++ data/data_01/glassbox.py
@@ -0,0 +1,17 @@
+from pylab import *
+import numpy as np
+
+data = loadtxt("glass.data",delimiter=",")
+head = data[data[:,10]==7]
+tableware = data[data[:,10]==6]
+containers = data[data[:,10]==5]
+
+print head[:,1]
+
+data =(containers[:,1], tableware[:,1], head[:,1])
+
+boxplot(data,0,'rs',0,0.75)
+
+show()
+
+
