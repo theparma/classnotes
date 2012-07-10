74040bdbf28208d832f7638fe1b69dacb54fa239
Burak Bayramli <burak@lieferheld.de>
2012-07-11 00:28:06 +0200

    -

--- stat/stat_06/build.py
+++ stat/stat_06/build.py
@@ -1,8 +1,8 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex mcmc.tex")
-    os.system("evince mcmc.pdf")
+    os.system("pdflatex stat*.tex")
+    os.system("evince stat*.pdf")
     exit()
     
     
