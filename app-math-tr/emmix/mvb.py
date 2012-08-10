c92d10b3eb51abbc27535fbe2b3829991a9e3b35
burak bayramli <burak@burak-Aspire-S3.(none)>
2012-08-10 15:15:04 +0200

    -

--- app-math-tr/emmix/mvb.py
+++ app-math-tr/emmix/mvb.py
@@ -7,10 +7,6 @@ K=3
 iter=20
 Y = np.loadtxt('binarydigits.txt')
 
-#for i in range(100):
-#    plt.imshow(Y[i,:].reshape((8,8),order='F'), cmap=plt.cm.gray)
-#    plt.show()
-
 attempts=20
 Lbest = -np.inf
 eps=1e-15
