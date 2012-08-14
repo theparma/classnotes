77f05bb110dac358658e8b59ef25bce4c9a003da
burak bayramli <burak@burak-Aspire-S3.(none)>
2012-08-14 11:41:56 +0200

    -

--- app-math-tr/emmix/build.py
+++ app-math-tr/emmix/build.py
@@ -1,7 +1,13 @@
-import os, sys
+import os, sys, re
+
+def run_command(command):
+    result = []
+    f = os.popen(command, "r")
+    sys.stdout.flush()
+    for l in f.xreadlines():
+        result.append(l)
+    return result
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex emmix.tex")
-    os.system("evince emmix.pdf")
-    exit()
-    
+    s = run_command("pdflatex emmix*.tex")
+    os.system("evince emmix*.pdf")    
