ac3a15aed58aaa6271983a2162161f0ff9dbbc31
burak bayramli <burak@burak-Aspire-S3.(none)>
2012-08-14 12:40:14 +0200

    -

--- app-math-tr/emmix/gauss.py
+++ app-math-tr/emmix/gauss.py
@@ -1,12 +1,14 @@
 import math, random, copy, sys
 import numpy as np
 
-def expectation_maximization(t, nbclusters=2, nbiter=3, normalize=False,\
-        epsilon=0.001, monotony=False, datasetinit=True):
+def expectation_maximization(t, nbclusters=2, nbiter=3, \
+                                 normalize=False,epsilon=0.001, \
+                                 monotony=False, datasetinit=True):
     def pnorm(x, m, s):
         """ 
-        Compute the multivariate normal distribution with values vector x,
-        mean vector m, sigma (variances/covariances) matrix s
+        Compute the multivariate normal distribution with values
+        vector x, mean vector m, sigma (variances/covariances) matrix
+        s
         """
         xmt = np.matrix(x-m).transpose()
         for i in xrange(len(s)):
@@ -14,14 +16,17 @@ def expectation_maximization(t, nbclusters=2, nbiter=3, normalize=False,\
                 s[i,i] = sys.float_info[3]
         sinv = np.linalg.inv(s)
         xm = np.matrix(x-m)
-        return (2.0*math.pi)**(-len(x)/2.0)*(1.0/math.sqrt(np.linalg.det(s)))\
-                *math.exp(-0.5*(xm*sinv*xmt))
+        return (2.0*math.pi)**(-len(x)/2.0)*\
+            (1.0/math.sqrt(np.linalg.det(s)))\
+            *math.exp(-0.5*(xm*sinv*xmt))
 
     def draw_params():
             if datasetinit:
-                tmpmu = np.array([1.0*t[random.uniform(0,nbobs),:]],np.float64)
+                tmpmu = np.array([1.0*t[random.uniform(0,nbobs),:]],
+                                 np.float64)
             else:
-                tmpmu = np.array([random.uniform(min_max[f][0], min_max[f][1])\
+                tmpmu = np.array([random.uniform(min_max[f][0],
+                                                 min_max[f][1])\
                         for f in xrange(nbfeatures)], np.float64)
             return {'mu': tmpmu,\
                     'sigma': np.matrix(np.diag(\
@@ -53,21 +58,18 @@ def expectation_maximization(t, nbclusters=2, nbiter=3, normalize=False,\
     Px = np.ndarray([nbobs,nbclusters], np.float64) # P(obs|clust)
     # iterate nbiter times searching for the best "quality" clustering
     for iteration in xrange(nbiter):
-        ##############################################
         # Step 1: draw nbclusters sets of parameters #
-        ##############################################
         params = [draw_params() for c in xrange(nbclusters)]
         old_log_estimate = sys.maxint         # init, not true/real
         log_estimate = sys.maxint/2 + epsilon # init, not true/real
         estimation_round = 0
-        # Iterate until convergence (EM is monotone) <=> < epsilon variation
+        # Iterate until convergence (EM is monotone) <=>
+        # < epsilon variation
         while (abs(log_estimate - old_log_estimate) > epsilon\
                 and (not monotony or log_estimate < old_log_estimate)):
             restart = False
             old_log_estimate = log_estimate
-            ########################################################
             # Step 2: compute P(Cluster|obs) for each observations #
-            ########################################################
             for o in xrange(nbobs):
                 for c in xrange(nbclusters):
                     # Px[o,c] = P(x|c)
@@ -88,17 +90,16 @@ def expectation_maximization(t, nbclusters=2, nbiter=3, normalize=False,\
                 Pclust[o,:] /= tmpSum
                 #assert math.fsum(Pclust[:,c]) >= 0.99 and\
                 #        math.fsum(Pclust[:,c]) <= 1.01
-            ###########################################################
             # Step 3: update the parameters (sets {mu, sigma, proba}) #
-            ###########################################################
             print "iter:", iteration, " estimation#:", estimation_round,\
                     " params:", params
             for c in xrange(nbclusters):
                 tmpSum = math.fsum(Pclust[:,c])
                 params[c]['proba'] = tmpSum/nbobs
-                if params[c]['proba'] <= 1.0/nbobs:           # restart if all
-                    restart = True                             # converges to
-                    print "Restarting, p:",params[c]['proba'] # one cluster
+                # restart if all converges to one cluster
+                if params[c]['proba'] <= 1.0/nbobs:  
+                    restart = True                   
+                    print "Restarting, p:",params[c]['proba'] 
                     break
                 m = np.zeros(nbfeatures, np.float64)
                 for o in xrange(nbobs):
@@ -106,14 +107,9 @@ def expectation_maximization(t, nbclusters=2, nbiter=3, normalize=False,\
                 params[c]['mu'] = m/tmpSum
                 s = np.matrix(np.diag(np.zeros(nbfeatures, np.float64)))
                 for o in xrange(nbobs):
-                    s += Pclust[o,c]*(np.matrix(t[o,:]-params[c]['mu']).transpose()*\
-                            np.matrix(t[o,:]-params[c]['mu']))
-                    #print ">>>> ", t[o,:]-params[c]['mu']
-                    #diag = Pclust[o,c]*((t[o,:]-params[c]['mu'])*\
-                    #        (t[o,:]-params[c]['mu']).transpose())
-                    #print ">>> ", diag
-                    #for i in xrange(len(s)) :
-                    #    s[i,i] += diag[i]
+                    s += Pclust[o,c]*\
+                    (np.matrix(t[o,:]-params[c]['mu']).transpose()*\
+                     np.matrix(t[o,:]-params[c]['mu']))
                 params[c]['sigma'] = s/tmpSum
                 print "------------------"
                 print params[c]['sigma']
@@ -122,23 +118,24 @@ def expectation_maximization(t, nbclusters=2, nbiter=3, normalize=False,\
             if not restart:
                 restart = True
                 for c in xrange(1,nbclusters):
-                    if not np.allclose(params[c]['mu'], params[c-1]['mu'])\
-                    or not np.allclose(params[c]['sigma'], params[c-1]['sigma']):
+                    if not np.allclose(params[c]['mu'],
+                                       params[c-1]['mu'])\
+                    or not np.allclose(params[c]['sigma'],
+                                       params[c-1]['sigma']):
                         restart = False
                         break
-            if restart:                # restart if all converges to only
-                old_log_estimate = sys.maxint          # init, not true/real
+            if restart:     # restart if all converges to only
+                old_log_estimate = sys.maxint     # init, not true/real
                 log_estimate = sys.maxint/2 + epsilon # init, not true/real
                 params = [draw_params() for c in xrange(nbclusters)]
                 continue
             ### /Test bound conditions and restart
 
-            ####################################
             # Step 4: compute the log estimate #
-            ####################################
             log_estimate = math.fsum([math.log(math.fsum(\
-                    [Px[o,c]*params[c]['proba'] for c in xrange(nbclusters)]))\
-                    for o in xrange(nbobs)])
+                    [Px[o,c]*params[c]['proba'] \
+                         for c in xrange(nbclusters)]))\
+                                          for o in xrange(nbobs)])
             print "(EM) old and new log estimate: ",\
                     old_log_estimate, log_estimate
             estimation_round += 1
