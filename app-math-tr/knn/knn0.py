e3ce8525dcfef339249d6ad0fbab3ad38ac8d94c
burak bayramli <burak@burak-Aspire-S3.(none)>
2012-12-28 16:25:01 +0100

    -

--- app-math-tr/knn/knn0.py
+++ app-math-tr/knn/knn0.py
@@ -4,11 +4,9 @@ import time
     
 
 if __name__ == "__main__": 
-    test = np.array([[3.,4.],[5.,5.],[9.,2.],[3.2,5.],[7.,5.],[8.,9.],[7.,6.],
-                     [8,4],[6,2]
-                     ])
-
-    for x in test: plt.plot(x[0],x[1],'o')
+    test = np.array([[3.,4.],[5.,5.],[9.,2.],[3.2,5.],[7.,5.],
+                     [8.,9.],[7.,6.],[8,4],[6,2],[2,2],[4,8]])
+    for x in test: plt.plot(x[0],x[1],'ko')
     plt.xlim(0,10)
     plt.ylim(0,10)
     plt.show()
