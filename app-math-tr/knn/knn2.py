8037c7e0bc3a30ffa360dab811998195618c2b83
burak bayramli <burak@burak-Aspire-S3.(none)>
2012-12-28 11:52:41 +0100

    -

--- app-math-tr/knn/knn2.py
+++ app-math-tr/knn/knn2.py
@@ -2,19 +2,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
+__rmin__ = 2
+
 def dist(vect,x):
     return np.sum(np.abs(x-vect),axis=1)
 
-# node = [centroid, radius, (child1,child2)]
+#
+# node = [pivot, radius, points, [child1,child2]]
+#
 def form_tree(points,node):
-    center = np.mean(points,axis=0)
-    print "center",center
-    idx = np.argmax(dist(points,center))
+    pivot = points[0]
+    print "pivot",pivot
+    radius = np.max(dist(points,pivot))
+    node[0] = pivot
+    node[1] = radius
+    if len(points) <= __rmin__:
+        node[2] = points
+        return
+    print "node",node
+    idx = np.argmax(dist(points,pivot))
     furthest = points[idx,:]
-    print "c1", furthest
+    print "new pivot 1", furthest
     idx = np.argmax(dist(points,furthest))
     furthest2 = points[idx,:]
-    print "c2", furthest2
+    print "new pivot 2", furthest2
     dist1=dist(points,furthest)
     dist2=dist(points,furthest2)
     diffs = dist1-dist2
@@ -22,11 +33,17 @@ def form_tree(points,node):
     print diffs
     p1 = points[diffs <= 0]
     p2 = points[diffs > 0]
-    print "p1", p1
-    print "p2", p2
-    
+    print "points 1", p1
+    print "points 2", p2
+    node[3][0] = [None,None,None,[None,None]]
+    node[3][1] = [None,None,None,[None,None]]
+    form_tree(p1,node[3][0])
+    form_tree(p2,node[3][1])
+                    
 
 if __name__ == "__main__": 
     test = np.array([[3.,4.],[5.,5.],[9.,2.],[3.2,5.],[7.,5.],[8.,9.],[7.,6.],
                      [8,4],[6,2]])
-    form_tree(test,[])
+    tree = [None,None,None,[None,None]]
+    form_tree(test,tree)
+    print tree
