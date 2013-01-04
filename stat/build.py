cd6a8a43b335ffecd21c3a15cb713f732715228c
burak bayramli <burak@burak-Aspire-S3.(none)>
2013-01-04 10:11:25 +0100

    -

--- stat/build.py
+++ stat/build.py
@@ -1,11 +1,12 @@
 import os, sys, glob
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdftk stat*/*.pdf output ~/Dropbox/Public/skfiles/stat.pdf")
+    os.system("pdftk stat_00/stat_00.pdf stat_01/stat_01.pdf stat_02/stat_02.pdf stat_03/stat_03.pdf stat_04/stat_04.pdf stat_05/stat_05.pdf stat_mcmc/stat_mcmc.pdf stat_coal/stat_coal.pdf stat_mixbern/stat_mixbern.pdf stat_normtable/stat_normtable.pdf output ~/Dropbox/Public/skfiles/stat.pdf")
     exit()
     
 if sys.argv[1] == 'all':
     for a in sorted(glob.glob("stat*")):
+        print a
         os.chdir(a)
         os.system("pdflatex stat*.tex")    
         os.chdir("..")
--- stat/stat_08/build.py
+++
@@ -1,5 +0,0 @@
-import os, sys, re
-
-if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex stat_08.tex")
-    os.system("evince stat_08.pdf")
