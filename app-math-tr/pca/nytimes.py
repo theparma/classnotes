435c3bed52ec580b2693a261e7527dab639487b7
burak bayramli <burak@burak-Aspire-S3.(none)>
2013-01-20 16:09:47 +0100

    -

---
+++ app-math-tr/pca/nytimes.py
@@ -0,0 +1,11 @@
+from pandas import *
+import numpy as np
+import scipy.linalg as linalg
+
+nyt = read_csv ("nytimes.csv")
+nyt2 = nyt.ix[:,2:].as_matrix()
+means = np.mean(nyt2, axis=0)
+meanless_nyt = nyt2 - means
+cov_nyt = np.cov(meanless_nyt,rowvar=0)
+eigs,eigv = linalg.eig(np.mat(cov_nyt))
+eig_ind = argsort(eigs)
