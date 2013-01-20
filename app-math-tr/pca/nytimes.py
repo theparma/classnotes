90d2a4ef9618de5536577da8b0792245e470306a
burak bayramli <burak@burak-Aspire-S3.(none)>
2013-01-20 16:12:43 +0100

    -

--- app-math-tr/pca/nytimes.py
+++ app-math-tr/pca/nytimes.py
@@ -7,5 +7,5 @@ nyt2 = nyt.ix[:,2:].as_matrix()
 means = np.mean(nyt2, axis=0)
 meanless_nyt = nyt2 - means
 cov_nyt = np.cov(meanless_nyt,rowvar=0)
-eigs,eigv = linalg.eig(np.mat(cov_nyt))
-eig_ind = argsort(eigs)
+eigs,eigv = linalg.eig(cov_nyt)
+#eig_ind = argsort(eigs)
