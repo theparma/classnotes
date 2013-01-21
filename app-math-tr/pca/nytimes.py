28197ad53f83ab77b2f4fc6adb7ac6e1457cda03
burak bayramli <burak@burak-Aspire-S3.(none)>
2013-01-22 00:23:31 +0100

    -

--- app-math-tr/pca/nytimes.py
+++ app-math-tr/pca/nytimes.py
@@ -4,5 +4,5 @@ import numpy.linalg as lin
 nyt = read_csv ("nytimes.csv")
 nyt = nyt.drop(["class.labels"],axis=1)
 nyt = nyt - nyt.mean(0)
-nytn = nyt.div(nyt.sum(1), axis=0)
-#u,s,v = lin.svd(nytn)
+nyt2 = nyt.div(nyt.sum(1), axis=0)
+nyt2.to_csv("/home/burak/nytimes3.csv")
