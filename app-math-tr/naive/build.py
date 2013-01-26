c1fd760cd06c2a93a09ab8f8984bea4b1f906fa5
burak bayramli <burak@burak-Aspire-S3.(none)>
2013-01-26 16:44:18 +0100

    -

--- app-math-tr/naive/build.py
+++ app-math-tr/naive/build.py
@@ -5,7 +5,3 @@ if len(sys.argv) == 1 or sys.argv[1] == 'tex':
     os.system("evince naive.pdf")
     exit()
     
-if sys.argv[1] == 'zip':
-    os.system("zip ~/Dropbox/Public/skfiles/naive.zip *.txt *.png *.py naive.tex naive.pdf")
-        
-
