8c8d930d4522b40565adf62384e4ee9c32e5b6c3
burak bayramli <burak@burak-Aspire-S3.(none)>
2013-02-07 11:11:56 +0100

    -

--- app-math-tr/naive/build.py
+++ app-math-tr/naive/build.py
@@ -1,7 +1,7 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex naive.tex")
-    os.system("evince naive.pdf")
+    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py naive2.ipynb -f latex")
+    os.system("pdflatex naive2.tex")
+    os.system("evince naive2.pdf")
     exit()
-    
