3a49144377495288e7d764edfce59f797fd49266
burak bayramli <burak@burak-Aspire-S3.(none)>
2013-02-12 15:12:28 +0100

    -

--- app-math-tr/build.py
+++ app-math-tr/build.py
@@ -16,7 +16,7 @@ spline/spline2.pdf ./ml-tr/ml-tr.pdf ./naive/naive2.pdf ./simplex/simplex.pdf \
 pde-wave-deriv/pde_01.pdf ./heat-deriv/heat-deriv.pdf ./heat/heat.pdf \
 ./curvature/curvature.pdf ./level/level2.pdf ./lk/lk.pdf ./varcalc/varcalc.pdf \
 ./filter/filter.pdf ./svdcluster/svdcluster2.pdf  ./svdrecom/svdrecom.pdf \
-./phd/phd.pdf \
+./regularization/regular.pdf ./phd/phd.pdf \
 output ~/Dropbox/Public/skfiles/app-math-tr.pdf "
     os.system(cmd)
     exit()
