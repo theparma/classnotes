61716a22041d3285ca6453b257c65d7b0f388da8
burak bayramli <burak@burak-Aspire-S3.(none)>
2013-02-16 08:19:30 +0100

    -

--- compscieng1/compscieng_1_ztransform/build.py
+++ compscieng1/compscieng_1_ztransform/build.py
@@ -1,7 +1,7 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py compscieng_1_80_2.ipynb -f latex")
-    os.system("pdflatex compscieng_1_80_2.tex")
-    os.system("evince compscieng_1_80_2.pdf")
+    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py compscieng_z.ipynb -f latex")
+    os.system("pdflatex compscieng_z.tex")
+    os.system("evince compscieng_z.pdf")
     exit()
