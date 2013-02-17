b0808529be26b66f55c52686882ca984f89165ad
burak bayramli <burak@burak-Aspire-S3.(none)>
2013-02-17 19:30:23 +0100

    -

--- stat/build.py
+++ stat/build.py
@@ -1,7 +1,7 @@
 import os, sys, glob
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdftk stat_00/stat_00.pdf stat_01/stat_01.pdf stat_02/stat_02.pdf stat_03/stat_03.pdf stat_04/stat_04.pdf stat_05/stat_05.pdf stat_mcmc/stat_mcmc.pdf stat_coal/stat_coal2.pdf stat_mixbern/stat_mixbern2.pdf stat_normtable/stat_normtable.pdf output ~/Dropbox/Public/skfiles/stat.pdf")
+    os.system("pdftk stat_00/stat_00.pdf stat_01/stat_01.pdf stat_02/stat_02.pdf stat_03/stat_03.pdf stat_04/stat_04.pdf stat_05/stat_05_2.pdf stat_mcmc/stat_mcmc.pdf stat_coal/stat_coal2.pdf stat_mixbern/stat_mixbern2.pdf stat_normtable/stat_normtable.pdf output ~/Dropbox/Public/skfiles/stat.pdf")
     exit()
     
 if sys.argv[1] == 'all':
