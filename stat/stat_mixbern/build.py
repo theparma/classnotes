a7d6c0b77af21786006c8d114ff069de1984dd5a
burak bayramli <burak@burak-Aspire-S3.(none)>
2013-02-17 12:19:06 +0100

    -

--- stat/stat_mixbern/build.py
+++ stat/stat_mixbern/build.py
@@ -1,5 +1,7 @@
-import os, sys, re
+import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex stat_08.tex")
-    os.system("evince stat_08.pdf")
+    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py stat_mixbern2.ipynb -f latex")
+    os.system("pdflatex stat_mixbern2.tex")
+    os.system("evince stat_mixbern2.pdf")
+    exit()
