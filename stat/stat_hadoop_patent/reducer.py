930a5c5d4dc39a8857dcb5eb1efabb7c16acbe2f
burak bayramli <burak@burak-Aspire-S3.(none)>
2013-02-24 20:30:36 +0100

    -

--- stat/stat_hadoop_patent/reducer.py
+++ stat/stat_hadoop_patent/reducer.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python
-import sys
-(last_key, sum, count) = (None, 0.0, 0)
-
-for line in sys.stdin:
-    (key, val) = line.split("\t")
-    if last_key and last_key != key:
-        print last_key + "\t" + str(sum / count)
-        (sum, count) = (0.0, 0)
-    last_key = key
-    sum += float(val)
-    count += 1
-print last_key + "\t" + str(sum / count)
+import os,sys
+os.environ['MPLCONFIGDIR']='/tmp' 
+import pandas as pd
+data = pd.read_csv(sys.stdin,sep="\t",names=['country','count'])
+grouped = data.groupby('country').mean()
+grouped.to_csv(sys.stdout,sep="\t",header=False)
