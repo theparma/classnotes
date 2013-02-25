d6ae3c42e1e464e1cb70c841bfb24e6bb4e67bbe
burak bayramli <burak@burak-Aspire-S3.(none)>
2013-02-25 15:10:07 +0100

    -

---
+++ app-math-tr/kmeans/build.py
@@ -0,0 +1,7 @@
+import os, sys
+
+if len(sys.argv) == 1 or sys.argv[1] == 'tex':
+    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py kmeans.ipynb -f latex")
+    os.system("pdflatex kmeans.tex")
+    os.system("evince kmeans.pdf")
+    exit()
