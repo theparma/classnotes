7b7cce75fae03aff250dd4ffcb5c5adc3aa5b608
burak bayramli <burak@burak-Aspire-S3.(none)>
2013-02-26 15:00:20 +0100

    -

--- stat/stat_hadoop_kmeans/runner.py
+++
@@ -1,9 +0,0 @@
-import os
-from pandas import *
-import numpy as np
-
-df = read_csv("synthetic.txt",names=['a','b'],sep="   ")
-centers = DataFrame(np.random.randn(10,2)) * np.array(df.mean())
-centers.to_csv("/tmp/centers.txt",header=None)
-os.system("ssh localhost -l hduser /home/hduser/Downloads/hadoop*/bin/hadoop dfs -copyFromLocal /tmp/centers.txt /user/hduser")
-os.system("ssh localhost -l hduser /home/hduser/Downloads/hadoop*/bin/hadoop dfs -cat /user/hduser/centers.txt")
