20f5167f5bb4529cd0bd7453ac72d8f163922e10
burak bayramli <burak@burak-Aspire-S3.(none)>
2013-02-26 14:02:39 +0100

    -

--- stat/stat_hadoop_kmeans/mapper.py
+++ stat/stat_hadoop_kmeans/mapper.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python
-import os,sys
+import os,sys,itertools
+import numpy as np
 from numpy import linalg as la
-
-def euclid(inA,inB):
-    return la.norm(inA - inB)
-
 os.environ['MPLCONFIGDIR']='/tmp' 
 import pandas as pd
-df = pd.read_csv(sys.stdin,names=['a','b'],sep="   ")
-centers = pd.read_csv("/tmp/centers.csv",sep=",")
+
+centers = pd.read_csv("/tmp/centers.csv",header=None,sep=",")
+print centers[:4]
+
+def dist(vect,x):
+    return np.fromiter(itertools.imap(np.linalg.norm, vect-x),dtype=np.float)
+
+def closest(x):
+    d = dist(np.array(centers)[:,1:3],np.array(x))
+    return np.argmin(d)
+
+df = pd.read_csv(sys.stdin,header=None,sep="   ")
+df['closest'] = df.apply(closest,axis=1)
+print df[:20]
