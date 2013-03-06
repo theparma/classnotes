59de843daac1b89a44a844b88ebfb9f4af26462b
burak bayramli <burak@burak-Aspire-S3.(none)>
2013-03-06 15:33:04 +0100

    -

--- stat/stat_hadoop_logreg/mapper.py
+++ stat/stat_hadoop_logreg/mapper.py
@@ -4,7 +4,6 @@ import numpy as np
 from numpy import linalg as la
 os.environ['MPLCONFIGDIR']='/tmp' 
 import pandas as pd
-from random import randrange
 
 def sigmoid(arr):
     return 1.0/(1+np.exp(-arr))
@@ -13,15 +12,15 @@ def stoc_grad_ascent0(data_mat, label_mat):
     m,n = data_mat.shape
     label_mat=label_mat.reshape((m,1))
     theta = np.ones((n,1))
-    alpha = 0.001
+    alpha = 0.1
     for i in range(m):
         h = sigmoid(np.sum(np.dot(data_mat[i],theta)))
         error = label_mat[i] - h
         theta = theta + alpha * data_mat[i].reshape((n,1)) * error
     return theta
 
-machine = str(randrange(10))
-data = pd.read_csv(sys.stdin,header=None,sep='\t')
-xy = np.array(data.ix[:,0:2]); labels = data.ix[:,2].T
-theta = np.array(stoc_grad_ascent0(xy,labels))
-print machine + "\t" + "\t".join([str(x) for x in theta[:,0]])
+df = pd.read_csv(sys.stdin,header=None,names=['x','y','labels'],sep='\t')
+df['intercept']=1.0
+data = df[['intercept','x','y']]
+theta = np.array(stoc_grad_ascent0(np.array(data),df['labels']))
+print "KEY1\t" + ",".join([str(x) for x in theta[:,0]])
--- stat/stat_hadoop_logreg/reducer.py
+++ stat/stat_hadoop_logreg/reducer.py
@@ -1,1 +1,15 @@
+#!/usr/bin/python
+import os,sys,itertools
+import numpy as np
+from numpy import linalg as la
+os.environ['MPLCONFIGDIR']='/tmp' 
+import pandas as pd
 
+def new_val(x):
+    return pd.Series(np.array(str(x).split(","),dtype=np.float64))
+
+df = pd.read_csv(sys.stdin,sep="\t",names=['key','val'])
+df2 = df['val'].apply(new_val)
+df3 = df.combine_first(df2)
+df4 =  df3.groupby('key').mean()
+df4.to_csv(sys.stdout, sep=',',header=None)
