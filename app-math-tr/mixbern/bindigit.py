9c0b968a60547d416c1a68d7c50d4f78a7a340a7
burak bayramli <burak@burak-Aspire-S3.(none)>
2013-03-07 17:45:07 +0100

    -

---
+++ app-math-tr/mixbern/bindigit.py
@@ -0,0 +1,13 @@
+import numpy as np
+from EMmixtureBernoulli import *
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+K=3
+iter=20
+Y = np.loadtxt('binarydigits.txt')
+print Y.shape
+
+for i in range(100):
+    plt.imshow(Y[i,:].reshape((8,8),order='C'), cmap=plt.cm.gray)
+    plt.show()
