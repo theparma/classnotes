39f88c45e5c1618a1ea7b8c0382e2e47675fe033
burak bayramli <burak@burak-Aspire-S3.(none)>
2013-03-20 13:05:56 +0100

    -

--- app-math-tr/build.py
+++ app-math-tr/build.py
@@ -3,17 +3,17 @@ import os, sys, glob
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
     cmd = "pdftk ./cover/cover.pdf ./model/model.pdf ./bases/bases.pdf \
 ./karesel/karesel.pdf ./quotient/quotient.pdf ./invtrig/invtrig.pdf \
-pfractions/pfractions.pdf ratio/ratio.pdf compos/compos.pdf \
-series/series.pdf poldiv/poldiv.pdf ./fundamental/fundamental.pdf  \
+./pfractions/pfractions.pdf ratio/ratio.pdf compos/compos.pdf \
+./series/series.pdf poldiv/poldiv.pdf ./fundamental/fundamental.pdf  \
 ./cauchy/cauchy.pdf ./integral-mult/integral-mult.pdf ./lhospital/lhospital.pdf ./euler/euler.pdf \
 ./intexp/intexp.pdf ./taylor/taylor.pdf ./taylor2d/taylor2d.pdf ./pca/pca.pdf \
  ./pca/pcaperf.pdf ./eig/eig.pdf ./logaritma/logaritma.pdf ./complexity/complexity.pdf \
 ./probsolve/probsolve.pdf ./id3/id3.pdf ./knn/knn2.pdf ./turev/turev.pdf \
 ./totaldiff/totaldiff.pdf ./eigseg/eigseg2.pdf ./rayleigh/rayleigh.pdf exp/exp.pdf \
-./moment/moment.pdf ./dagilimlar/dagilimlar2.pdf ./buyuk/buyuk.pdf ./cebisev/cebisev.pdf \
-spline/spline2.pdf ./ml-tr/ml-tr.pdf ./logreg/logreg.pdf ./kmeans/kmeans.pdf ./naive/naive2.pdf  \
+./dagilimlar/dagilimlar2.pdf ./buyuk/buyuk.pdf ./cebisev/cebisev.pdf \
+./spline/spline2.pdf ./ml-tr/ml-tr.pdf ./logreg/logreg.pdf ./kmeans/kmeans.pdf ./naive/naive2.pdf  \
 ./simplex/simplex.pdf ./qp/qp.pdf ./svm/svm2.pdf ./fem/fem2.pdf ./fourier/fourier2.pdf \
-pde-wave-deriv/pde_01.pdf ./heat-deriv/heat-deriv.pdf ./heat/heat2.pdf \
+./pde-wave-deriv/pde_01.pdf ./heat-deriv/heat-deriv.pdf ./heat/heat2.pdf \
 ./curvature/curvature.pdf ./level/level2.pdf ./lk/lk2.pdf ./varcalc/varcalc.pdf \
 ./filter/filter.pdf ./svdcluster/svdcluster2.pdf  ./svdrecom/svdrecom.pdf \
 ./regularization/regular.pdf ./mixbern/stat_mixbern2.pdf ./meanshift/meanshift.pdf ./phd/phd.pdf \
