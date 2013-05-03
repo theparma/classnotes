d22e5d30ac079cbf9ed55407fa78c8e818bb9122
Burak Bayramli <burak@burak-Aspire-S3.(none)>
2013-05-03 08:40:47 +0200

    -

--- stat/stat_hadoop_kmeans/build.py
+++ stat/stat_hadoop_kmeans/build.py
@@ -1,8 +1,7 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py kmeans.ipynb -f latex")
-    #os.system("cp kmeans-diag.png kmeans_files/kmeans_fig_00.png")
+    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py  --format latex kmeans.ipynb")
     os.system("pdflatex kmeans.tex")
     os.system("evince kmeans.pdf")
-    exit()
+    #exit()
