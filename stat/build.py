b4a98e7520c909c059d37c7fe58e6ea898b68b93
Burak Bayramli <burak@burak-Aspire-S3.(none)>
2013-07-31 17:28:30 +0200

    -

--- stat/build.py
+++ stat/build.py
@@ -4,7 +4,7 @@ if len(sys.argv) == 1 or sys.argv[1] == 'tex':
     os.system("""
     pdftk stat_00/stat_00.pdf stat_01/stat_01.pdf stat_02/stat_02.pdf \
     stat_03/stat_03.pdf stat_04/stat_04.pdf stat_05/stat_05_2.pdf \
-    stat_mcmc/stat_mcmc.pdf stat_coal/stat_coal2.pdf \
+    stat_06/stat_06.pdf stat_mcmc/stat_mcmc.pdf stat_coal/stat_coal2.pdf \
     stat_pandas/div.pdf stat_pandas_ratings/ratings.pdf \
     stat_wells/stat_wells.pdf stat_hadoop_patent/patent.pdf \
     stat_hadoop_kmeans/kmeans.pdf stat_hadoop_logreg/stat_hadoop_logreg.pdf \
