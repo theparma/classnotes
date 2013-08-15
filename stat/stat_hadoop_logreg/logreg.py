d29ea110cabf16f7b7dee626d660eda91d941d96
burak bayramli <bbayramli@bbayramli-OptiPlex-7010.(none)>
2013-08-15 15:55:20 +0200

    fixes

--- stat/stat_hadoop_logreg/logreg.py
+++ stat/stat_hadoop_logreg/logreg.py
@@ -4,6 +4,7 @@ Logistic regression for map/reduce written for MRJob.
 from mrjob.job import MRJob
 from mrjob.protocol import PickleProtocol
 import numpy as np
+import os, thread
 
 class MRLogisticRegression(MRJob):
     INTERNAL_PROTOCOL = PickleProtocol
@@ -11,12 +12,14 @@ class MRLogisticRegression(MRJob):
         super(MRLogisticRegression, self).__init__(*args, **kwargs)
         self.n  = 1
         self.m = 3
-        self.theta = np.ones((self.m,1))
+        self.theta = 2 * np.ones((self.m,1))
+        #self.key = str(thread.get_ident()) + "-" + str(os.getpid())
 
     def sigmoid(self, arr):
         return 1.0/(1+np.exp(-arr))
 
     def stoc_grad_ascent0(self, data_mat, label, theta):
+        #alpha = 0.003
         alpha = 0.001
         for j in range(self.m):
             h = self.sigmoid(np.dot(data_mat,theta)[0])
@@ -27,8 +30,10 @@ class MRLogisticRegression(MRJob):
         tokens = map(np.float,line.split('\t'))
         data = np.append(1.0,np.array(tokens[:-1]))
         label = np.array(tokens[-1])
-        theta = self.stoc_grad_ascent0(data, label, self.theta)
-        yield ('key1', theta)
+        self.theta = self.stoc_grad_ascent0(data, label, self.theta)
+        
+    def mapper_final(self):        
+        yield ("key1", self.theta)
                 
     def reducer(self, key, tokens):
         theta = None
@@ -36,7 +41,7 @@ class MRLogisticRegression(MRJob):
             if theta != None: theta = (theta + val) / 2.
             else: theta = val.copy()
         yield('result',str(theta))
-
+        
 if __name__ == '__main__':
     MRLogisticRegression.run()
     
