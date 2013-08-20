ab99060f7f77463950c544c17d01df04dd5bac96
burak <burak@burak-Satellite-U300.(none)>
2013-08-20 11:26:19 +0200

    kmeans fixed

--- stat/stat_hadoop_kmeans/kmeans.py
+++ stat/stat_hadoop_kmeans/kmeans.py
@@ -1,9 +1,8 @@
 from mrjob.job import MRJob
 from mrjob.protocol import PickleProtocol
-from mrjob.protocol import RawProtocol
 import numpy as np, sys
 import pandas as pd
-import os, random
+import os, random, ast
 
 def euc_to_clusters(x,y):
     return np.sqrt(np.sum((x-y)**2, axis=1))
@@ -14,6 +13,9 @@ class MRKMeans(MRJob):
     def __init__(self, *args, **kwargs):
         super(MRKMeans, self).__init__(*args, **kwargs)
         self.centers_ = pd.read_csv("/tmp/centers.csv",header=None,sep="   ")
+        self.k = 15
+        self.new_centers = np.zeros((self.k,2))
+        self.counts = np.zeros((self.k,))
         
     def mapper(self, key, line):
         point = np.array(map(np.float,line.split('   ')))
@@ -21,24 +23,21 @@ class MRKMeans(MRJob):
         yield(c, point)
                         
     def reducer(self, key, tokens):
-        def sc(x):
-            if len(x) == 1: return "0" + x
-            return x
-        new_centers = {}
         for val in tokens:
-            if new_centers.get(key) == None:
-                new_centers[key] = val
-            else:
-                new_centers[key] = (new_centers[key] + val) / 2. 
-        for k in new_centers.keys():
-            yield(sc(str(k)),str(new_centers[k]))
+            self.new_centers[key] += val
+            self.counts[key] += 1
+        yield('final', self.new_centers[key] / self.counts[key])
         
-    def reduce_2(self, key, values):
+    def reduce_all_centers(self, key, values):
+        self.f=open("/tmp/centers.csv","w")
         for val in values:
-            print "---", val
-
+            self.f.write("   ".join(map(str,val)))
+            self.f.write("\n")
+        self.f.close()
+        
     def steps(self):
         return [self.mr(mapper=self.mapper,reducer=self.reducer),
-                self.mr(reducer=self.reduce_2)  ]
+                self.mr(reducer=self.reduce_all_centers)]
+    
 if __name__ == '__main__':
-    MRKMeans.run()
+    for i in range(10): MRKMeans.run()
