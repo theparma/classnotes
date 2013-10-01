40f58a39db15f444b17ca18257d9767a5149e3ae
burak <burak@burak-Aspire-V5-122P.(none)>
2013-10-01 22:38:02 +0200

    -

--- stat/stat_hadoop_meanshift/build.py
+++
@@ -1,7 +0,0 @@
-import os, sys
-
-if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py meanshift.ipynb -f latex")
-    os.system("pdflatex meanshift.tex")
-    os.system("evince meanshift.pdf")
-    exit()
