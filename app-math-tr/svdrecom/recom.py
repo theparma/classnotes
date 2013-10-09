92474704b7816240b1fb9396f5ca3bcce0657b46
burak <burak@burak-Aspire-V5-122P.(none)>
2013-10-09 16:52:14 +0200

    -

---
+++ app-math-tr/svdrecom/recom.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy.linalg as lin
+import numpy as np
+import matplotlib.pyplot as plt
+
+#df = pd.read_csv("/home/burak/Downloads/movielens2.csv",sep=',')
+df = pd.read_csv("/home/burak/Downloads/movielens1.csv",sep=',')
+#U,S,VT = lin.svd(df)
+#print S
+#plt.plot(U[:,0], U[:,1],'.')
+#plt.show()
+
