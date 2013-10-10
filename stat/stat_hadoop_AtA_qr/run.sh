643cd2d6d2a87661ce6cb56a21992dca4929cc33
burak <burak@burak-Aspire-V5-122P.(none)>
2013-10-10 15:35:11 +0200

    -

--- stat/stat_hadoop_AtA_qr/run.sh
+++ stat/stat_hadoop_AtA_qr/run.sh
@@ -1,6 +1,5 @@
 #ssh localhost -l hduser  python /home/burak/Documents/classnotes/stat/stat_hadoop_AtA_qr/Ax.py  hdfs:///user/A_matrix -r hadoop  --jobconf mapred.map.tasks=1 --jobconf mapred.reduce.tasks=1
-
-python Ax.py A_matrix --runner=local
-
+#python Ax.py A_matrix --runner=local
 #hadoop fsck / -delete
 
+python AtB.py A_matrix1 B_matrix1 --runner=local
--- stat/stat_hadoop_rnd_svd/run.sh
+++ stat/stat_hadoop_rnd_svd/run.sh
@@ -2,4 +2,5 @@
 #tail -n+2 $HOME/Downloads/movielens1.csv | head -20 | python mrproj.py  > $HOME/Downloads/movielens2.csv
 #python mrr.py ~/Downloads/movielens2.csv  > R
 #python mrq.py ~/Downloads/movielens2.csv --file R > Q
-python mr_q_ur.py Q --file R > U
+#python mr_q_ur.py Q --file R > U
+python mraq.py ~/Downloads/movielens2.csv Q > BT
