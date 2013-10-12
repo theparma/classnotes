4427a063cf77e141856a424c593710d14a0f1560
burak <burak@burak-Aspire-V5-122P.(none)>
2013-10-12 10:07:19 +0200

    -

--- stat/stat_hadoop_rnd_svd/run.sh
+++ stat/stat_hadoop_rnd_svd/run.sh
@@ -1,4 +1,5 @@
 DIM=7
+#python tabify.py /home/burak/Downloads/movielens2.csv A.dat
 python tabify.py w1.csv A.dat
 python mrproj.py A.dat --k=$DIM > Y.dat
 python mrr.py Y.dat  --n=$DIM > R.dat # cholesky R
