5c62484c2eb4a48f2062187df3fda8fd083e6246
burak <burak@burak-Aspire-V5-122P.(none)>
2013-10-14 14:32:32 +0200

    -

--- stat/stat_hadoop_rnd_svd/netflix/common.py
+++ stat/stat_hadoop_rnd_svd/netflix/common.py
@@ -10,7 +10,6 @@ def line_to_coo(line, dim):
     tokens = line.split(",")
     line_sps = sparse.lil_matrix((1,dim))
     for tok in tokens:
-        tmp = tok.split(":")
-        line_sps[ 0,int(tmp[0]) ] = float(tmp[1])
+        tmp = tok.split(":"); line_sps[ 0,int(tmp[0]) ] = float(tmp[1])
     return line_sps.tocoo()
     
--- stat/stat_hadoop_rnd_svd/netflix/run.sh
+++ stat/stat_hadoop_rnd_svd/netflix/run.sh
@@ -3,4 +3,4 @@ DIR=/home/burak/Downloads/netflix/download
 #python run.py
 #python transpose.py $DIR/netflix1_reorg.csv > $DIR/netflix1_reorg_user_movie.csv 
 #python mrproj.py $DIR/A.dat --k=$DIM > $DIR/Y.dat
-python mrr.py $DIR/Y.dat  --k=$DIM > $DIR/R.dat # cholesky R
+python ../mrr.py $DIR/Y.dat  --k=$DIM > $DIR/R.dat # cholesky R
