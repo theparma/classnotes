3c6603edc15fd73f36ef5b42aefaff5e9566888d
burak <burak@burak-Aspire-V5-122P.(none)>
2013-10-14 13:42:59 +0200

    -

--- stat/stat_hadoop_rnd_svd/netflix/run.sh
+++ stat/stat_hadoop_rnd_svd/netflix/run.sh
@@ -1,2 +1,3 @@
 #python run.py
-python transpose.py /home/burak/Downloads/netflix/download/netflix1_reorg.csv > /home/burak/Downloads/netflix/download/netflix1_reorg_user_movie.csv 
+#python transpose.py /home/burak/Downloads/netflix/download/netflix1_reorg.csv > /home/burak/Downloads/netflix/download/netflix1_reorg_user_movie.csv 
+python mrproj.py /home/burak/Downloads/netflix/download/A.dat --k=7
