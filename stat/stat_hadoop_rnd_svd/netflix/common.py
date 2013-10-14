30494d32f6d9f2d28a314112b44d928b23993cca
burak <burak@burak-Aspire-V5-122P.(none)>
2013-10-14 13:43:03 +0200

    -

---
+++ stat/stat_hadoop_rnd_svd/netflix/common.py
@@ -0,0 +1,16 @@
+from mrjob.job import MRJob
+from mrjob.protocol import PickleProtocol
+from mrjob.protocol import RawValueProtocol
+from mrjob.protocol import RawProtocol
+import numpy as np, sys, itertools
+from scipy import sparse
+
+def line_to_coo(line, dim):
+    line = line.replace('"','')
+    tokens = line.split(",")
+    line_sps = sparse.lil_matrix((1,dim))
+    for tok in tokens:
+        tmp = tok.split(":")
+        line_sps[ 0,int(tmp[0]) ] = float(tmp[1])
+    return line_sps.tocoo()
+    
