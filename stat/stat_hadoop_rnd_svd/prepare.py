87fa243379af24b55436d74bd8e1585208ecd7a7
burak <burak@burak-Aspire-V5-122P.(none)>
2013-10-14 16:46:14 +0200

    -

--- stat/stat_hadoop_rnd_svd/prepare.py
+++ stat/stat_hadoop_rnd_svd/prepare.py
@@ -9,7 +9,7 @@ fin = open (sys.argv[1])
 fout = open (sys.argv[2],"w")
 for x in fin.readlines():
     tokens = x.split(';')
-    res = [str(i)+":"+str(x) for i,x in enumerate(tokens[1:]) if x != None]
+    res = [str(i)+":"+str(x) for i,x in enumerate(tokens[1:]) if x != '' ]
     fout.write( tokens[0] + '\t' + ";".join(map(str,res )))
 
 fout.close()
