74209b26acad8902df703eabccfed1e8e9d48be8
burak <burak@burak-Aspire-V5-122P.(none)>
2013-10-14 16:39:22 +0200

    -

--- stat/stat_hadoop_rnd_svd/prepare.py
+++ stat/stat_hadoop_rnd_svd/prepare.py
@@ -1,8 +1,9 @@
 import sys, os
 
 '''
-Add a tab after the key (first column) and pick not null
-*only* indexing them with the column position
+Add a tab after the key (first column) and pick non-null columns
+*only* and indexing them with the column position as index:value
+seperated by semicolon
 '''
 fin = open (sys.argv[1])
 fout = open (sys.argv[2],"w")
