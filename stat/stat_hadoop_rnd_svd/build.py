6d66f26f9db1a0f6e0759c08e037504feaeb5a10
burak <burak@burak-Aspire-V5-122P.(none)>
2013-10-15 23:21:44 +0200

    -

--- stat/stat_hadoop_rnd_svd/build.py
+++ stat/stat_hadoop_rnd_svd/build.py
@@ -3,6 +3,8 @@ import os, sys
 if sys.argv[1] == 'zip':
     os.system("zip %s/Downloads/mr_rnd_svd_0.2.zip *.py *.csv *matrix* " % os.environ['HOME'])
 if sys.argv[1] == 'send':
-    os.system("scp -r * burak@host2:/home/burak/stat_hadoop_rnd_svd")
+    os.system("scp -r * hduser@host2:/home/hduser/stat_hadoop_rnd_svd")
+if sys.argv[1] == 'send-local':
+    os.system("cp -r * /home/hduser/stat_hadoop_rnd_svd")
 if sys.argv[1] == 'sfile':
     os.system("scp -r /home/burak/Downloads/netflix burak@host2:/home/burak/Downloads")
