e44ea6190d0114d3a85e0e2e3a8ab3fdd8b70277
burak <burak@burak-Aspire-V5-122P.(none)>
2013-10-16 17:00:00 +0200

    -

--- stat/stat_hadoop_rnd_svd/run.sh
+++ stat/stat_hadoop_rnd_svd/run.sh
@@ -2,10 +2,12 @@ DIM=7
 #N=30
 N=3730
 #python prepare.py /home/burak/Downloads/movielens_small/movielens2.csv A.dat
-python prepare.py w1.csv A.dat 
-python mrproj.py A.dat --k=$DIM --n=$N --file mrc.py --runner=local > Y.dat
-python mrr.py Y.dat  --k=$DIM > R.dat # cholesky R
-python mrq.py Y.dat --R=R.dat --file R.dat  > Q.dat 
-python mraq.py A.dat Q.dat --k=$DIM --n=$N  --file mrc.py  > BT.dat
-python mrr.py BT.dat  --k=$DIM > R_BT.dat
-python mr_q_uhat.py Q.dat --R=R_BT.dat --file R_BT.dat > U_final.dat
+#python prepare.py /home/burak/Downloads/movielens2.csv A.dat
+#python prepare.py w1.csv A.dat 
+#python mrproj.py A.dat --k=$DIM --n=$N --file mrc.py --runner=local > Y.dat
+#python mrr.py Y.dat  --k=$DIM > R.dat # cholesky R
+#python mrq.py Y.dat --R=R.dat --file R.dat  > Q.dat
+pig -x local join.pig > AQ.dat
+#python mraq.py A.dat Q.dat --k=$DIM --n=$N  --file mrc.py  > BT.dat
+#python mrr.py BT.dat  --k=$DIM > R_BT.dat
+#python mr_q_uhat.py Q.dat --R=R_BT.dat --file R_BT.dat > U_final.dat
