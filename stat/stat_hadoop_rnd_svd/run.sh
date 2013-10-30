e026021ee4d321539fca2667f2abc4ba3aa871c6
burak <burak@burak-Aspire-V5-122P.(none)>
2013-10-30 14:02:23 +0100

    -

--- stat/stat_hadoop_rnd_svd/run.sh
+++ stat/stat_hadoop_rnd_svd/run.sh
@@ -9,10 +9,10 @@ N=3730
 #N=17771
 #python prepare.py /home/burak/Downloads/movielens2.csv A.dat # large
 #python prepare.py /home/burak/Downloads/movielens_SMALL/movielens2.csv A.dat # medium
-#python prepare.py w1.csv A.dat # small
-python mrproj.py A.dat --k=$DIM --n=$N --file mrc.py --runner=local > Y.dat
-python mrr.py Y.dat  --k=$DIM > R.dat # cholesky R
-python mrq.py Y.dat --R=R.dat --file R.dat  > Q.dat
+python prepare.py w1.csv A.dat # small
+#python mrproj.py A.dat --k=$DIM --n=$N --file mrc.py --runner=local > Y.dat
+#python mrr.py Y.dat  --k=$DIM > R.dat # cholesky R
+#python mrq.py Y.dat --R=R.dat --file R.dat  > Q.dat
 python mraq.py A.dat Q.dat --k=$DIM --n=$N  --file mrc.py --runner=local > BT.dat
 python mrr.py BT.dat  --k=$DIM > R_BT.dat
 python mr_q_uhat.py Q.dat --R=R_BT.dat --file R_BT.dat > U_final.dat
