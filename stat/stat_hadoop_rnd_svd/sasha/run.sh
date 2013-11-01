eb16f0025adaff90faafa89c1fd9cdef144715b3
burak <burak@burak-Aspire-V5-122P.(none)>
2013-11-01 22:03:17 +0100

    -

--- stat/stat_hadoop_rnd_svd/sasha/run.sh
+++ stat/stat_hadoop_rnd_svd/sasha/run.sh
@@ -1,14 +1,11 @@
 DIR1=/home/burak/Downloads/sasha/node1/
-DIR2=/home/burak/Downloads/sasha/node2/
 RESPONSE=tcp://localhost:5040
 CONF=local
-#split ../A.dat -n l/2; mv xaa $DIR1/A.dat; mv xab $DIR2/A.dat
-#cp ../A.dat $DIR1/
+cp ../A.dat $DIR1/
 python proj.py $CONF -i A.dat -o Y.dat  -r $RESPONSE
-#python ata.py $CONF -i Y.dat -o YtY.dat -r $RESPONSE
-#cat $DIR1/YtY.dat $DIR2/YtY.dat > YtY.dat 
-#cp $DIR1/YtY.dat YtY.dat # combine
-#sort YtY.dat > YtY_sorted.dat # sort the results by key
-#python chol.py YtY_sorted.dat /tmp/R.dat
-#python a_inv_r.py $CONF -i Y.dat -o Q.dat  -f /tmp/R.dat -r $RESPONSE
+python ata.py $CONF -i Y.dat -o YtY.dat -r $RESPONSE
+cp $DIR1/YtY.dat YtY.dat # combine
+sort YtY.dat > YtY_sorted.dat # sort the results by key
+python chol.py YtY_sorted.dat /tmp/R.dat
+python a_inv_r.py $CONF -i Y.dat -o Q.dat  -f /tmp/R.dat -r $RESPONSE
 python join.py $CONF -i A.dat,Q.dat -o AQ.dat -r $RESPONSE
