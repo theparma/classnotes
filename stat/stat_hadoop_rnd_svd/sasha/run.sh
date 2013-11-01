07f6f13fc82d4ea917c390fa27136b0cbc0edf05
burak <burak@burak-Aspire-V5-122P.(none)>
2013-11-01 13:56:21 +0100

    -

--- stat/stat_hadoop_rnd_svd/sasha/run.sh
+++ stat/stat_hadoop_rnd_svd/sasha/run.sh
@@ -2,5 +2,5 @@ DIR1=/home/burak/Downloads/sasha/node1/
 #python proj.py local -l ../A.dat -o Y.dat
 #python ata.py local -i Y.dat -o YtY.dat
 #sort $DIR1/YtY.dat > $DIR1/YtY_sorted.dat
-python chol.py $DIR1/YtY_sorted.dat $DIR1/R.dat
+#python chol.py $DIR1/YtY_sorted.dat $DIR1/R.dat
 
