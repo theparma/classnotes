468ecd81a4f78f42f1839d3ecea74f028fa0b8a2
burak <burak@burak-Aspire-V5-122P.(none)>
2013-11-01 21:04:30 +0100

    -

--- stat/stat_hadoop_rnd_svd/sasha/run.sh
+++ stat/stat_hadoop_rnd_svd/sasha/run.sh
@@ -1,6 +1,10 @@
 DIR1=/home/burak/Downloads/sasha/node1/
-python proj.py local -l ../A.dat -o Y.dat  -r tcp://localhost:5040
-python ata.py local -i Y.dat -o YtY.dat -r tcp://localhost:5040
-sort $DIR1/YtY.dat > $DIR1/YtY_sorted.dat
-python chol.py $DIR1/YtY_sorted.dat /tmp/R.dat -r tcp://localhost:5040
-python a_inv_r.py local -i Y.dat -o Q.dat  -f /tmp/R.dat -r tcp://localhost:5040
+DIR2=/home/burak/Downloads/sasha/node2/
+CONF=local2
+#split ../A.dat -n l/2; mv xaa $DIR1/A.dat; mv xab $DIR2/A.dat
+python proj.py $CONF -i A.dat -o Y.dat  -r tcp://localhost:5040
+python ata.py $CONF -i Y.dat -o YtY.dat -r tcp://localhost:5040
+cat $DIR1/YtY.dat $DIR2/YtY.dat > YtY.dat 
+sort YtY.dat > YtY_sorted.dat
+python chol.py YtY_sorted.dat /tmp/R.dat
+python a_inv_r.py $CONF -i Y.dat -o Q.dat  -f /tmp/R.dat -r tcp://localhost:5040
