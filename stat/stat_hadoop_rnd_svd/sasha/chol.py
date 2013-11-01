49a36885311f2136925862ea4110134d016068ca
burak <burak@burak-Aspire-V5-122P.(none)>
2013-11-01 12:39:32 +0100

    -

---
+++ stat/stat_hadoop_rnd_svd/sasha/chol.py
@@ -0,0 +1,16 @@
+from sasha import job
+import numpy as np, sys, itertools
+from scipy import sparse
+import random, re, sys, proj
+import numpy.linalg as lin
+
+arr = []
+fin = open(sys.argv[1])
+for x in fin.readlines():
+    [id,row] = x.strip().split('\t')
+    arr.append(map(np.float,row.split(';')))
+    
+res =  np.array(arr)
+print res.shape; print res
+print lin.cholesky(res)
+    
