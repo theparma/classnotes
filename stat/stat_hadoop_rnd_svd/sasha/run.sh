8635360ebf738908060f103b7c055a84ea63f51f
burak <burak@burak-Aspire-V5-122P.(none)>
2013-11-01 22:15:06 +0100

    -

--- stat/stat_hadoop_rnd_svd/sasha/run.sh
+++ stat/stat_hadoop_rnd_svd/sasha/run.sh
@@ -1,11 +1,12 @@
 DIR1=/home/burak/Downloads/sasha/node1/
 RESPONSE=tcp://localhost:5040
 CONF=local
-cp ../A.dat $DIR1/
-python proj.py $CONF -i A.dat -o Y.dat  -r $RESPONSE
-python ata.py $CONF -i Y.dat -o YtY.dat -r $RESPONSE
-cp $DIR1/YtY.dat YtY.dat # combine
-sort YtY.dat > YtY_sorted.dat # sort the results by key
-python chol.py YtY_sorted.dat /tmp/R.dat
-python a_inv_r.py $CONF -i Y.dat -o Q.dat  -f /tmp/R.dat -r $RESPONSE
-python join.py $CONF -i A.dat,Q.dat -o AQ.dat -r $RESPONSE
+#cp ../A.dat $DIR1/
+#python proj.py $CONF -i A.dat -o Y.dat  -r $RESPONSE
+#python ata.py $CONF -i Y.dat -o YtY.dat -r $RESPONSE
+#cp $DIR1/YtY.dat YtY.dat # combine
+#sort YtY.dat > YtY_sorted.dat # sort the results by key
+#python chol.py YtY_sorted.dat /tmp/R.dat
+#python a_inv_r.py $CONF -i Y.dat -o Q.dat  -f /tmp/R.dat -r $RESPONSE
+#python join.py $CONF -i A.dat,Q.dat -o AQ.dat -r $RESPONSE
+python a_t_q.py $CONF -i AQ.dat -o AtQ.dat -r $RESPONSE
