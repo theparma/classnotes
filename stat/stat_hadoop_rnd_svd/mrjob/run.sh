d6f85b95bef8d378b21799b270331c239d671d32
burak <burak@burak-Aspire-V5-122P.(none)>
2013-11-03 02:15:20 +0100

    -

--- stat/stat_hadoop_rnd_svd/mrjob/run.sh
+++ stat/stat_hadoop_rnd_svd/mrjob/run.sh
@@ -1,15 +1,8 @@
-# 4
-#  local:
-#    base_tmp_dir: /mnt/ram
-# medium, 45 sec
 date
 DIM=7
 N=30
 #N=3730
 #N=17771
-#python prepare.py /home/burak/Downloads/movielens2.csv A.dat # large
-#python prepare.py /home/burak/Downloads/movielens_SMALL/movielens2.csv A.dat # medium
-python prepare.py ../w1.csv A.dat # small
 python mrproj.py A.dat --k=$DIM --n=$N --file mrc.py --runner=local > Y.dat
 python mrr.py Y.dat  --k=$DIM > R.dat # cholesky R
 python mrq.py Y.dat --R=R.dat --file R.dat  > Q.dat
