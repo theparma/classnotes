45a2c8260aa666721d1aacf5c42b7e9112ed7d94
burak <burak@burak-Aspire-V5-122P.(none)>
2013-11-03 17:33:22 +0100

    -

--- stat/stat_hadoop_rnd_svd/sasha/run_two.sh
+++ stat/stat_hadoop_rnd_svd/sasha/run_two.sh
@@ -11,13 +11,9 @@ python chol.py YtY_sorted.dat /tmp/R.dat
 python a_inv_r.py $CONF -i Y.dat -o Q.dat  -f /tmp/R.dat -r $RESPONSE
 python join.py $CONF -i A.dat,Q.dat -o AQ.dat -r $RESPONSE
 python atq.py $CONF -i AQ.dat -o BT.dat -r $RESPONSE
-cat $DIR1/BT.dat $DIR2/BT.dat > BT.dat 
-sort -h BT.dat > $DIR1/BT_sorted.dat	
-sort -h BT.dat > $DIR2/BT_sorted.dat	
-python ata.py $CONF -i BT_sorted.dat -o BTB.dat -r $RESPONSE
+python ata.py $CONF -i BT.dat -o BTB.dat -r $RESPONSE
 cat $DIR1/BTB.dat $DIR2/BTB.dat > BTB.dat
-sort -h BTB.dat > $DIR1/BTB_sorted.dat
-sort -h BTB.dat > $DIR2/BTB_sorted.dat
-python chol.py $DIR1/BTB_sorted.dat /tmp/R_BT.dat
+sort -h BTB.dat > BTB_sorted.dat
+python chol.py BTB_sorted.dat /tmp/R_BT.dat
 python q_uhat.py $CONF -i Q.dat -o U_final.dat  -f /tmp/R_BT.dat -r $RESPONSE
 cat $DIR1/U_final.dat $DIR2/U_final.dat > /tmp/U_final.dat
