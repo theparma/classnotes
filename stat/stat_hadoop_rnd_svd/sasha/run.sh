47e761778a77bfbf33d774d86db63c4744c9e0fc
burak <burak@burak-Aspire-V5-122P.(none)>
2013-11-06 20:30:31 +0100

    -

--- stat/stat_hadoop_rnd_svd/sasha/run.sh
+++ stat/stat_hadoop_rnd_svd/sasha/run.sh
@@ -3,11 +3,11 @@ date
 #CONF=cluster2
 #RESPONSE=tcp://chost1:5040
 
-#CONF=cluster3
-#RESPONSE=tcp://host3:5040
+CONF=cluster1
+RESPONSE=tcp://host3:5040
 
-CONF=local2
-RESPONSE=tcp://localhost:5040
+#CONF=local2
+#RESPONSE=tcp://localhost:5040
 
 # cleanup
 #rm /tmp/x-*
@@ -24,7 +24,7 @@ RESPONSE=tcp://localhost:5040
 # cp ../A.dat $HOME/Downloads/sasha/node1/A.dat
 
 # two server small
-# python $HOME/Documents/sasha/servers.py split-copy $CONF ../A.dat A.dat
+#python $HOME/Documents/sasha/servers.py split-copy $CONF ../A.dat A.dat
 
 # two server movielens
 # python $HOME/Documents/sasha/servers.py split-copy $CONF $HOME/Downloads/movielens_withids.csv A.dat
@@ -37,19 +37,19 @@ RESPONSE=tcp://localhost:5040
 # processing
 #
 
-python proj.py $CONF -i A.dat -o Y.dat  -r $RESPONSE
-python ata.py $CONF -i Y.dat -o YtY.dat -r $RESPONSE
-python $HOME/Documents/sasha/servers.py pull-combine $CONF YtY.dat
-sort /tmp/YtY.dat > /tmp/YtY_sorted.dat # sort the results by key
-python chol.py /tmp/YtY_sorted.dat /tmp/R.dat
-python a_inv_r.py $CONF -i Y.dat -o Q.dat  -f /tmp/R.dat -r $RESPONSE
-python join.py $CONF -i A.dat,Q.dat -o AQ.dat -r $RESPONSE
+# python proj.py $CONF -i A.dat -o Y.dat  -r $RESPONSE
+# python ata.py $CONF -i Y.dat -o YtY.dat -r $RESPONSE
+# python $HOME/Documents/sasha/servers.py pull-combine $CONF YtY.dat
+# sort /tmp/YtY.dat > /tmp/YtY_sorted.dat # sort the results by key
+# python chol.py /tmp/YtY_sorted.dat /tmp/R.dat
+# python a_inv_r.py $CONF -i Y.dat -o Q.dat  -f /tmp/R.dat -r $RESPONSE
+# python join.py $CONF -i A.dat,Q.dat -o AQ.dat -r $RESPONSE
 python atq.py $CONF -i AQ.dat -o BT.dat -r $RESPONSE
-python ata.py $CONF -i BT.dat -o BTB.dat -r $RESPONSE
-python $HOME/Documents/sasha/servers.py pull-combine $CONF BTB.dat
-sort -h /tmp/BTB.dat > /tmp/BTB_sorted.dat
-python chol.py /tmp/BTB_sorted.dat /tmp/R_BT.dat
-python q_uhat.py $CONF -i Q.dat -o U_final.dat  -f /tmp/R_BT.dat -r $RESPONSE
-python $HOME/Documents/sasha/servers.py pull-combine $CONF U_final.dat
+# python ata.py $CONF -i BT.dat -o BTB.dat -r $RESPONSE
+# python $HOME/Documents/sasha/servers.py pull-combine $CONF BTB.dat
+# sort -h /tmp/BTB.dat > /tmp/BTB_sorted.dat
+# python chol.py /tmp/BTB_sorted.dat /tmp/R_BT.dat
+# python q_uhat.py $CONF -i Q.dat -o U_final.dat  -f /tmp/R_BT.dat -r $RESPONSE
+# python $HOME/Documents/sasha/servers.py pull-combine $CONF U_final.dat
 
 date
