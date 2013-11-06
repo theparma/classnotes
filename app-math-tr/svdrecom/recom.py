a1fcfcb92249d92d7e6c5556b972a07e113b656c
burak <burak@burak-Aspire-V5-122P.(none)>
2013-11-06 14:24:09 +0100

    -

--- app-math-tr/svdrecom/recom.py
+++ app-math-tr/svdrecom/recom.py
@@ -3,10 +3,12 @@ import numpy.linalg as lin
 import numpy as np
 import matplotlib.pyplot as plt
 
-#df = pd.read_csv("/home/burak/Downloads/movielens2.csv",sep=',')
-df = pd.read_csv("/home/burak/Downloads/movielens1.csv",sep=',')
-#U,S,VT = lin.svd(df)
-#print S
-#plt.plot(U[:,0], U[:,1],'.')
-#plt.show()
+import pandas as pd
+import numpy.linalg as lin
+df = pd.read_csv("/home/burak/Downloads/movielens1.csv",sep=';')
+print 'loaded'
+U,S,VT = lin.svd(np.array(df.ix[:,1:]))
+print 'svd'
+plt.plot(U[:,0], U[:,1],'.')
+plt.show()
 
