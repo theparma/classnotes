aac2406aa6dc5a56b2312f4c8025fcf57c7cf28c
burak <burak@burak-Aspire-V5-122P.(none)>
2013-11-28 15:40:12 +0100

    -

---
+++ app-math-tr/dynp/dp2.py
@@ -0,0 +1,28 @@
+from functools import wraps
+
+def memo(func):
+    cache = {}                                  
+    @wraps(func)                                
+    def wrap(*args):                            
+        if args not in cache: cache[args] = func(*args)           
+        return cache[args]                      
+    return wrap                                 
+
+DAG = {
+    'a': {'b':2, 'f': 9},
+    'b': {'d':2, 'c':1, 'f': 6},
+    'c': {'d':7},
+    'd': {'e':2, 'f': 3},
+    'e': {'f':4},
+    'f': {}
+}
+
+def rec_dag_sp(W, s, t):                        # Shortest path from s to t
+    @memo                                       # Memoize f
+    def d(u):                                   # Distance from u to t
+        if u == t: return 0                     # We're there!
+        return min(W[u][v]+d(v) for v in W[u])  # Best of every first step
+    return d(s)                                 # Apply f to actual start node
+
+print rec_dag_sp(DAG, 'a', 'f')
+
---
+++ app-math-tr/dynp/dp4.py
@@ -0,0 +1,49 @@
+class WeightedGraph(object):
+    def __init__(self,g):
+        self.g = g        
+    def weight(self,u,v):
+        return self.g[u][v]    
+    def itervertices(self):
+        return self.g.keys()
+    def neighbors(self, u):
+        return self.g[u].keys()
+    
+class ShortestPathResult(object):
+    def __init__(self):
+        self.d = {}
+        self.parent = {}
+    
+def shortest_path_bottomup(graph, s):
+    result = ShortestPathResult()
+    for v in graph.itervertices():
+        result.d[v] = float('inf')
+        result.parent[v] = None
+    result.d[s] = 0
+    for v in graph.g.keys():
+        print 'v=', v
+        for w in graph.neighbors(v):
+            print '   w=', w
+            new_distance = result.d[v] + graph.weight(v, w)
+            if result.d[w] > new_distance:
+                result.d[w] = new_distance
+                result.parent[w] = v                
+    return result
+
+if __name__ == "__main__": 
+ 
+    DAG = {
+        'a': {'b':2, 'f': 9},
+        'b': {'d':2, 'c':1, 'f': 6},
+        'c': {'d':7},
+        'd': {'e':2, 'f': 3},
+        'e': {'f':4},
+        'f': {}
+        }
+
+    graph = WeightedGraph(DAG)
+    
+#    path = shortest_path(graph, 'a')
+#    print path.d, path.parent
+    
+    path = shortest_path_bottomup(graph, 'a')
+    print path.d, path.parent
