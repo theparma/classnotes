d6f633f88962b4ae30e6d4f1c1ee7682daf52fab
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-02 16:24:05 +0100

    -

--- app-math-tr/level/build.py
+++ app-math-tr/level/build.py
@@ -1,7 +1,6 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py level2.ipynb -f latex")
-    os.system("pdflatex level2.tex")
-    os.system("evince level2.pdf")
+    os.system("pdflatex -shell-escape level.tex")
+    os.system("evince level.pdf")
     exit()
