05eb71ada6c10087a45ff2d455227930458da09e
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-02 09:54:06 +0100

    -

--- app-math-tr/dynp/build.py
+++ app-math-tr/dynp/build.py
@@ -1,7 +1,7 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex dynp.tex")
+    os.system("pdflatex  -shell-escape dynp.tex")
     os.system("evince dynp.pdf")
     exit()
         
