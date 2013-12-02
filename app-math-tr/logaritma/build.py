4e512c3f530e4148da25230a16cc2d686e1483ec
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-02 17:25:37 +0100

    -

--- app-math-tr/logaritma/build.py
+++ app-math-tr/logaritma/build.py
@@ -1,11 +1,8 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex logaritma.tex")
+    os.system("pdflatex -shell-escape logaritma.tex")
     os.system("evince logaritma.pdf")
     exit()
-
-if sys.argv[1] == 'zip':
-    os.system("zip ~/Dropbox/Public/skfiles/logaritma.zip logaritma.pdf logaritma.tex build.py *.jpg")
     
     
