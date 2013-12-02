22f9be114a8cfd2684c66a0668bf3d2933085419
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-02 20:45:50 +0100

    -

--- app-math-tr/svm/build.py
+++ app-math-tr/svm/build.py
@@ -1,7 +1,6 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py svm2.ipynb -f latex")
-    os.system("pdflatex svm2.tex")
-    os.system("evince svm2.pdf")
+    os.system("pdflatex -shell-escape svm.tex")
+    os.system("evince svm.pdf")
     exit()
