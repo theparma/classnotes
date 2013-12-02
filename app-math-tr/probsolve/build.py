6c47cc34fe928badb90f289b6ff9f7c3f5e779e5
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-02 14:24:35 +0100

    -

--- app-math-tr/probsolve/build.py
+++ app-math-tr/probsolve/build.py
@@ -1,12 +1,8 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex probsolve.tex")
+    os.system("pdflatex -shell-escape probsolve.tex")
     os.system("evince probsolve.pdf")
     exit()
 
-if sys.argv[1] == 'zip':    
-    cmd = "zip ~/Dropbox/Public/skfiles/probsolve.zip *.jpg *.png *.lisp *.pdf probsolve.tex build.py"
-    os.system(cmd)
-    
     
