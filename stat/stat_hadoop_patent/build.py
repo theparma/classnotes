a735c6b5f874c72fd4ea1eb1cead8e74ec029355
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-03 00:40:14 +0100

    -

--- stat/stat_hadoop_patent/build.py
+++ stat/stat_hadoop_patent/build.py
@@ -1,7 +1,6 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py patent.ipynb -f latex")
-    os.system("pdflatex patent.tex")
+    os.system("pdflatex -shell-escape patent.tex")
     os.system("evince patent.pdf")
     exit()
