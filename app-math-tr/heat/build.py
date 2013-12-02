294dcc68d2fecc7ae0974c007bbe7a99f9a15aa2
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-02 15:19:27 +0100

    -

--- app-math-tr/heat/build.py
+++ app-math-tr/heat/build.py
@@ -1,7 +1,6 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py heat2.ipynb -f latex")
-    os.system("pdflatex heat2.tex")
-    os.system("evince heat2.pdf")
+    os.system("pdflatex -shell-escape heat.tex")
+    os.system("evince heat.pdf")
     exit()
