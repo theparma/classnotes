bcc8e6df6ad00730af0f34f095a1c0da51a8ea11
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-02 16:53:23 +0100

    -

--- app-math-tr/fem/build.py
+++ app-math-tr/fem/build.py
@@ -1,7 +1,6 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py fem2.ipynb -f latex")
-    os.system("pdflatex fem2.tex")
-    os.system("evince fem2.pdf")
+    os.system("pdflatex -shell-escape fem.tex")
+    os.system("evince fem.pdf")
     exit()
