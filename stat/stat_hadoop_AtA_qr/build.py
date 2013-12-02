bc3af4fd46d4879ed355328c444f6194c53a111d
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-03 00:05:21 +0100

    -

--- stat/stat_hadoop_AtA_qr/build.py
+++ stat/stat_hadoop_AtA_qr/build.py
@@ -1,6 +1,5 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py  --format latex stat_AtA_qr.ipynb")
-    os.system("pdflatex stat_AtA_qr.tex")
+    os.system("pdflatex -shell-escape stat_AtA_qr.tex")
     os.system("evince stat_AtA_qr.pdf")
--- stat/stat_hadoop_AtA_qr/run.sh
+++
@@ -1,7 +0,0 @@
-#ssh localhost -l hduser  python /home/burak/Documents/classnotes/stat/stat_hadoop_AtA_qr/Ax.py  hdfs:///user/A_matrix -r hadoop  --jobconf mapred.map.tasks=1 --jobconf mapred.reduce.tasks=1
-#python Ax.py A_matrix --runner=local
-#hadoop fsck / -delete
-
-python AtB.py A_matrix1 B_matrix1 --k=2
-
-#python mraq.py A_matrix1 B_matrix1 > AtB_test_out.dat
