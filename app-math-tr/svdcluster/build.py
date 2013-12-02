35077dc44ac8c7448d10d15786edecf1537f8242
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-02 22:36:20 +0100

    -

--- app-math-tr/svdcluster/build.py
+++ app-math-tr/svdcluster/build.py
@@ -1,7 +1,6 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py svdcluster2.ipynb -f latex")
-    os.system("pdflatex svdcluster2.tex")
-    os.system("evince svdcluster2.pdf")
+    os.system("pdflatex -shell-escape svdcluster.tex")
+    os.system("evince svdcluster.pdf")
     exit()
