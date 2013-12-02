3321e867c9f23b6ebe0fa5bb775afdbd7272e72b
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-02 19:52:33 +0100

    -

--- app-math-tr/meanshift/build.py
+++ app-math-tr/meanshift/build.py
@@ -1,7 +1,6 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py meanshift.ipynb -f latex")
-    os.system("pdflatex meanshift.tex")
+    os.system("pdflatex -shell-escape meanshift.tex")
     os.system("evince meanshift.pdf")
     exit()
