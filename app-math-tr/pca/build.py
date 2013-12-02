6d11f3cf1448424d9f924faac974550781d04ad7
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-02 21:28:02 +0100

    -

--- app-math-tr/pca/build.py
+++ app-math-tr/pca/build.py
@@ -1,9 +1,6 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py pca.ipynb -f latex")
-    os.system("pdflatex pca.tex")
-    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py pcaperf.ipynb -f latex")
-    os.system("pdflatex pcaperf.tex")
+    os.system("pdflatex -shell-escape pca.tex")
     os.system("evince pca.pdf")
     exit()
