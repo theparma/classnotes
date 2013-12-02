14412899e406bbbf9b439310f964d05b744d46ed
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-02 10:14:23 +0100

    -

--- app-math-tr/id3/build.py
+++ app-math-tr/id3/build.py
@@ -1,12 +1,9 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex id3.tex")
+    os.system("pdflatex  -shell-escape id3.tex")
     os.system("evince id3.pdf")
     exit()
 
-if sys.argv[1] == 'zip':    
-    cmd = "zip ~/Dropbox/Public/skfiles/id3.zip build.py id3.pdf id3.tex *.jpg *.lisp "
-    os.system(cmd)
     
     
