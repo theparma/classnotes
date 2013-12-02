2c820253f55b617b7955229e9365c77c94017017
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-02 01:11:09 +0100

    -

--- app-math-tr/logreg/build.py
+++ app-math-tr/logreg/build.py
@@ -1,7 +1,6 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py logreg.ipynb -f latex")
-    os.system("pdflatex logreg.tex")
-    os.system("evince logreg.pdf")
+    os.system("pdflatex logreg2.tex")
+    os.system("evince logreg2.pdf")
     exit()
