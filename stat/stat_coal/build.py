1fa7b3dede2c990b0010f3dc31c335b9c8eddee8
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-03 00:22:14 +0100

    -

--- stat/stat_coal/build.py
+++ stat/stat_coal/build.py
@@ -1,7 +1,6 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py stat_coal2.ipynb -f latex")
-    os.system("pdflatex stat_coal2.tex")
-    os.system("evince stat_coal2.pdf")
+    os.system("pdflatex -shell-escape stat_coal.tex")
+    os.system("evince stat_coal.pdf")
     exit()
