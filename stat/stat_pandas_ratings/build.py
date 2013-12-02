29b0d4f1480fc1f48b960973d718fb91647d98ec
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-03 00:12:55 +0100

    -

--- stat/stat_pandas_ratings/build.py
+++ stat/stat_pandas_ratings/build.py
@@ -1,7 +1,6 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py  --format latex ratings.ipynb")
-    os.system("pdflatex ratings.tex")
+    os.system("pdflatex -shell-escape ratings.tex")
     os.system("evince ratings.pdf")
-    #exit()
+    exit()
