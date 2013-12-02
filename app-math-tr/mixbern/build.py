08d6a06f46fc0dae6bedeb251a6da71e7f8b87b2
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-02 21:52:05 +0100

    -

--- app-math-tr/mixbern/build.py
+++ app-math-tr/mixbern/build.py
@@ -1,7 +1,6 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py stat_mixbern2.ipynb -f latex")
-    os.system("pdflatex stat_mixbern2.tex")
-    os.system("evince stat_mixbern2.pdf")
+    os.system("pdflatex -shell-escape mixbern.tex")
+    os.system("evince mixbern.pdf")
     exit()
