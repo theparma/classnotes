6829c7e2dc1688b8b9d4362fbe391927d7a37dc2
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-02 09:41:23 +0100

    -

--- app-math-tr/logreg/build.py
+++ app-math-tr/logreg/build.py
@@ -1,6 +1,6 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex logreg2.tex")
+    os.system("pdflatex -shell-escape logreg2.tex")
     os.system("evince logreg2.pdf")
     exit()
