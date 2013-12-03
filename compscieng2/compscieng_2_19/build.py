ce011c2d51e22b5df74d61d3813c02b76b0593f9
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-03 13:01:16 +0100

    -

--- compscieng2/compscieng_2_19/build.py
+++ compscieng2/compscieng_2_19/build.py
@@ -1,12 +1,7 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex compscieng*.tex")
+    os.system("pdflatex -shell-escape compscieng*.tex")
     os.system("evince compscieng*.pdf")
     exit()
-    
-if sys.argv[1] == 'zip':    
-    cmd = "zip ~/Dropbox/Public/skfiles/compscieng_1/compscieng_1_%d.zip compscieng_1_%d.tex compscieng_1_%d.pdf *.png *.py" % \
-        (int(sys.argv[2]), int(sys.argv[2]), int(sys.argv[2]))
-    os.system(cmd)
     
