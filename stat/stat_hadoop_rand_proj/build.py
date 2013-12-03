098865e7a810f560b392c5814be16a1c538d4f5d
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-03 10:51:37 +0100

    -

--- stat/stat_hadoop_rand_proj/build.py
+++ stat/stat_hadoop_rand_proj/build.py
@@ -1,6 +1,5 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py  --format latex stat_hadoop_rand_proj.ipynb")
-    os.system("pdflatex stat_hadoop_rand_proj.tex")
+    os.system("pdflatex -shell-escape stat_hadoop_rand_proj.tex")
     os.system("evince stat_hadoop_rand_proj.pdf")
