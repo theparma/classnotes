955f9e034c903a31c9dcb780dbdbd8da452130b6
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-03 11:11:11 +0100

    -

--- stat/stat_05/build.py
+++ stat/stat_05/build.py
@@ -1,7 +1,6 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py stat_05_2.ipynb -f latex")
-    os.system("pdflatex stat_05_2.tex")
-    os.system("evince stat_05_2.pdf")
+    os.system("pdflatex -shell-escape stat_05.tex")
+    os.system("evince stat_05.pdf")
     exit()
