7601b66a43b7a3d8f0b710cf33726cdded1b2dff
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-03 19:51:33 +0100

    -

--- ode/ode_mattuck_01/build.py
+++ ode/ode_mattuck_01/build.py
@@ -1,12 +1,6 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex ode_mattuck_*.tex")
+    os.system("pdflatex -shell-escape ode_mattuck_*.tex")
     os.system("evince ode_mattuck_*.pdf")
     exit()
-    
-if sys.argv[1] == 'zip':    
-    cmd = "zip ~/Dropbox/Public/skfiles/ode/ode_mattuck_%d.zip *.png *.py ode_mattuck_%d.tex ode_mattuck_%d.pdf" % \
-        (int(sys.argv[2]), int(sys.argv[2]), int(sys.argv[2]))
-    os.system(cmd)
-    
