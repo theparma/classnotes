1ad01593f30fdf825de2898e7b2cafc65d4c9f5e
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-03 14:09:06 +0100

    -

--- calc_multi/calc_multi_06/1i3.py
+++ calc_multi/calc_multi_06/1i3.py
@@ -1,8 +1,6 @@
 from pylab import *
 import time
 
-plt.ion()
-
 xmax = 10.
 xmin = -10.
 D = 20
@@ -10,16 +8,16 @@ x = linspace(xmin, xmax, D)
 xlim(-10,10)
 ylim(-10,10)
 
-for t in linspace(-3., 3., 30):
-    plot (x,((2. - x)/2.))
-    plt.hold(True)
-    xx=2*cos(t)**2
-    yy=sin(t)**2
-    quiver(0,0,xx,yy)
-    plt.hold(True)
-    plot(xx,yy,'rd')
-    plt.hold(True)
-    plt.draw()
-    time.sleep(1)
-    plt.hold(False)
+for i,t in enumerate(linspace(-3., 3., 30)):
+    if i % 4 == 0: 
+        plot (x,((2. - x)/2.))
+        plt.hold(True)
+        xx=2*cos(t)**2
+        yy=sin(t)**2
+        quiver(0,0,xx,yy)
+        plt.hold(True)
+        plot(xx,yy,'rd')
+        plt.hold(True)
+        plt.savefig('1i3_' + str(i) + '.png')
+        plt.hold(False)
 
