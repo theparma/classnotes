66d1fa7f5dd2dfb786912cd2b6417904a48bce85
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-03 09:55:40 +0100

    -

--- stat/stat_wells/build.py
+++ stat/stat_wells/build.py
@@ -1,7 +1,6 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("python /home/burak/Downloads/nbconvert/nbconvert.py stat_wells.ipynb -f latex")
-    os.system("pdflatex stat_wells.tex")
+    os.system("pdflatex -shell-escape stat_wells.tex")
     os.system("evince stat_wells.pdf")
     exit()
