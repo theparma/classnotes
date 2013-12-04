b1b30399a1ea141afe68c0055b9a9e3c95595adc
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-04 05:48:04 +0100

    -

--- app-math-tr/varcalc/build.py
+++ app-math-tr/varcalc/build.py
@@ -1,6 +1,6 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex varcalc.tex")
+    os.system("pdflatex -shell-escape varcalc.tex")
     os.system("evince varcalc.pdf")
     exit()
