876fdba5dc1ef9f11402d88ab8f47443bcf69f93
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-05 10:17:55 +0100

    -

--- app-math-tr/knn/plot_circles.py
+++ app-math-tr/knn/plot_circles.py
@@ -1,6 +1,5 @@
 from matplotlib.patches import Circle
 import matplotlib.pyplot as plt
-import pprint
 import numpy as np
 import time
 import dist
@@ -18,44 +17,37 @@ def circle(x,rad,ax):
 
 def plot_points(pts,color,ax):
     for x in pts: ax.plot(x[0],x[1],color)
-    
-def form_tree(points,node,all_points):
+
+def plot_circles(pivot, radius, points, all_points):
     global i
-    print 'entering form tree ---'
-    print "points",points
-    pivot = points[0]
-    print "pivot",pivot
-    radius = np.max(dist.dist(points,pivot))
-    print "radius",radius
     f = plt.figure()
     ax = f.gca()
     plot_points(all_points,'ko',ax)
     plot_points(points,'ro',ax)
-    circle(pivot,radius,ax)
-    #plt.show()
-    plt.savefig('knn%s.png' % str(i))
     i += 1
+    circle(pivot,radius,ax)
+    plt.savefig('knn%s.png' % str(i))
+    
+    
+def form_tree(points,node,all_points):
+    pivot = points[0]
+    radius = np.max(dist.dist(points,pivot))
+    plot_circles(pivot, radius, points, all_points)
+    
     node[0] = pivot
     node[1] = radius
     if len(points) <= __rmin__:
         node[2] = points
         return
-    print "node",node
     idx = np.argmax(dist.dist(points,pivot))
     furthest = points[idx,:]
-    print "new pivot 1", furthest
     idx = np.argmax(dist.dist(points,furthest))
     furthest2 = points[idx,:]
-    print "new pivot 2", furthest2
     dist1=dist.dist(points,furthest)
     dist2=dist.dist(points,furthest2)
     diffs = dist1-dist2
-    print "points",points
-    print diffs
     p1 = points[diffs <= 0]
     p2 = points[diffs > 0]
-    print "points 1", p1
-    print "points 2", p2
     node[3][0] = new_node() # left child
     node[3][1] = new_node() # right child
     form_tree(p1,node[3][0],all_points)
@@ -67,7 +59,4 @@ if __name__ == "__main__":
                        [8.,9.],[7.,6.],[8,4],[6,2]])
     tree = new_node()
     form_tree(points,tree,points)
-    pp = pprint.PrettyPrinter(indent=4)
-    print "\ntree"
-    pp.pprint(tree)
     
