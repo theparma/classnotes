0ecf4ce28dec790e19408cb185a1d19822b0b30b
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-05 18:38:04 +0100

    -

--- ode/ode_mattuck_04/build.py
+++ ode/ode_mattuck_04/build.py
@@ -1,6 +1,6 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex ode_mattuck_*.tex")
+    os.system("pdflatex -shell-escape ode_mattuck_*.tex")
     os.system("evince ode_mattuck_*.pdf")
     exit()
