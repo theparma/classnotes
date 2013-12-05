2c4f9d1ca0ac9078b35a4dc4ccaf1e1bba68be45
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-05 21:14:15 +0100

    -

--- app-math-tr/complexity/build.py
+++ app-math-tr/complexity/build.py
@@ -1,6 +1,6 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex complexity.tex")
+    os.system("pdflatex -shell-escape complexity.tex")
     os.system("evince complexity.pdf")
     exit()
