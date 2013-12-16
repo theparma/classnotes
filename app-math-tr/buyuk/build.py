7044bd86808598ad1f10a5056bf1af43c2767397
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-16 11:52:39 +0100

    -

--- app-math-tr/buyuk/build.py
+++
@@ -1,6 +0,0 @@
-import os, sys
-
-if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex buyuk.tex")
-    os.system("evince  buyuk.pdf")
-    exit()
--- app-math-tr/cebisev/build.py
+++
@@ -1,6 +0,0 @@
-import os, sys
-
-if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex cebisev.tex")
-    os.system("evince  cebisev.pdf")
-    exit()
