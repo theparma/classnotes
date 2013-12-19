288b276916078adb6277fe838c624e1cecac3b78
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-19 15:23:22 +0100

    -

--- app-math-tr/eig/build.py
+++ app-math-tr/eig/build.py
@@ -1,7 +1,7 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex eig.tex")
+    os.system("pdflatex -shell-escape eig.tex")
     os.system("evince eig.pdf")
     exit()
 
