fc4864d4b1b310de5abd7754cc20b750b2ee30d5
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-25 23:45:58 +0100

    -

--- app-math-tr/fundamental/build.py
+++ app-math-tr/fundamental/build.py
@@ -1,7 +1,7 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex fund*.tex")
+    os.system("pdflatex -shell-escape fund*.tex")
     os.system("evince fund*.pdf")
     exit()
     
