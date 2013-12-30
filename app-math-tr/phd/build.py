f9b488be4e3ca8ac324081560721ba4d7add5f05
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-30 22:08:59 +0100

    -

--- app-math-tr/phd/build.py
+++ app-math-tr/phd/build.py
@@ -1,6 +1,6 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex phd.tex")
+    os.system("pdflatex -shell-escape phd.tex")
     os.system("evince phd.pdf")
     exit()
