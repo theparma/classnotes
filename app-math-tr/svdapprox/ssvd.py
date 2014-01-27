07c3c9c247d509a1fdda879c55fe893a511c200c
burak <burak@burak-Aspire-V5-122P.(none)>
2014-01-28 00:32:18 +0100

    -

--- app-math-tr/svdapprox/ssvd.py
+++ app-math-tr/svdapprox/ssvd.py
@@ -1,13 +1,12 @@
 from numpy.linalg import linalg as la
 import scipy.sparse as sps, itertools
-from pandas import *
 import numpy as np
 import pandas as pd, os
 
 def ssvd(df_train):
     gamma = 0.005 # regularization
     lam = 0.02
-    mu = 0.1
+    mu = df_train.mean().mean()
     m,n = df_train.shape
     print m,n
     k = 3 # rank
@@ -28,27 +27,9 @@ def ssvd(df_train):
             b_i[i] = b_i[i] + gamma * (e_ui - lam*b_i[i])
             q_i[:,i] = q_i[:,i] + gamma * (e_ui*p_u[u,:].T - lam*q_i[:,i])
             p_u[u,:] = p_u[u,:] + gamma * (e_ui*q_i[:,i].T - lam*p_u[u,:])
-
-    return b_u,b_i,q_i,p_u
+    return mu,b_u,b_i,q_i,p_u
             
 if __name__ == "__main__": 
-
-    d =  np.array(
-         [[5, 5, np.nan, 5],
-          [5, np.nan, 3, 4],
-          [3, 4, np.nan, 3],
-          [np.nan, np.nan, 5, 3],
-          [5, 4, 4, 5],
-          [5, 4, 5, 5]])
-
-    data = DataFrame (d.T,
-        columns=['0','1','2','3','4','5'],
-        index=['Ben','Tom','John','Fred'])
-    b_u,b_i,q_i,p_u = ssvd(data)
-    print b_u
-    print b_i
-    print q_i
-    print p_u
 
     df = pd.read_csv("%s/Downloads/movielens.csv" % os.environ['HOME'] ,sep=';')
     print df.shape
@@ -58,7 +39,7 @@ if __name__ == "__main__":
     df_train = df.ix[:5000,:]
     df_test = df.ix[5001:,:]
 
-    b_u,b_i,q_i,p_u = ssvd(df_train)
+    mu,b_u,b_i,q_i,p_u = ssvd(df_train)
     print b_u[10]
     
     
