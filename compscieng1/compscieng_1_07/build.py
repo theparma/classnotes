bb106cc7464bbf112e679bc7db6e4e580f4517f8
burak <burak@burak-Aspire-V5-122P.(none)>
2014-01-30 22:29:27 +0100

    -

--- compscieng1/compscieng_1_07/build.py
+++ compscieng1/compscieng_1_07/build.py
@@ -1,6 +1,6 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex compscieng_1_*.tex")
+    os.system("pdflatex -shell-escape compscieng_1_*.tex")
     os.system("evince compscieng_1_*.pdf")
     exit()
