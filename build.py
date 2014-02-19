59af45234295b0179986c2d7439b810273c06cb5
burak <burak@burak-Aspire-V5-122P.(none)>
2014-02-19 21:48:10 +0100

    -

--- build.py
+++ build.py
@@ -3,3 +3,4 @@ import os, sys
 if sys.argv[1] == 'clean':
     os.system("find . -name _region_* | xargs rm -rf ")
     os.system("find . -name '*.log' | xargs rm -rf ")
+    os.system("find . -name '*.aux' | xargs rm -rf ")
