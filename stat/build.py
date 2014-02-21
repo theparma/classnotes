9b44ca6fbca9cbaa2d66cf5fbfb92ac96f9a506d
burak <burak@burak-Aspire-V5-122P.(none)>
2014-02-21 19:55:30 +0100

    -

--- stat/build.py
+++ stat/build.py
@@ -2,7 +2,7 @@ import os, sys, glob
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
     os.system("""
-    pdftk stat_00/stat_00.pdf intro/intro.pdf stat_summary/stat_summary.pdf \
+    pdftk stat_00/stat_00.pdf stat_intro/intro.pdf stat_summary/stat_summary.pdf \
     stat_tests/stat_tests.pdf ./stat_sample_size/stat_sample_size.pdf \
     stat_mcmc/stat_mcmc.pdf stat_coal/stat_coal.pdf \
     stat_gauss_fusion/stat_gauss_fusion.pdf stat_pandas_ratings/ratings.pdf  \
