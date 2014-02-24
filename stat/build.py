fa172dad0191610cb5032d15925ee040832e619d
burak <burak@burak-Aspire-V5-122P.(none)>
2014-02-24 15:52:22 +0100

    -

--- stat/build.py
+++ stat/build.py
@@ -1,20 +1,28 @@
 import os, sys, glob
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("""
-    pdftk stat_00/stat_00.pdf stat_intro/intro.pdf stat_sampling*/stat_sampling*.pdf \
-    stat_summary/stat_summary.pdf stat_tests/stat_tests.pdf \
-    ./stat_sample_size/stat_sample_size.pdf \
-    stat_mcmc/stat_mcmc.pdf stat_coal/stat_coal.pdf \
-    stat_gauss_fusion/stat_gauss_fusion.pdf stat_pandas_ratings/ratings.pdf  \
-    stat_factor/stat_factor.pdf stat_wells/stat_wells.pdf \
+    cmd = """
+    pdftk \
+    ./stat_00/stat_00.pdf \
+    ./stat_intro/intro.pdf \
+    ./stat_summary/stat_summary.pdf \
+    ./stat_tests/stat_tests.pdf ./stat_sample_size/stat_sample_size.pdf \
+    ./stat_sampling_dist/stat_sampling_dist.pdf \
+    ./stat_mcmc/stat_mcmc.pdf \
+    ./stat_coal/stat_coal.pdf \
+    ./stat_gauss_fusion/stat_gauss_fusion.pdf \
+    ./stat_pandas_ratings/ratings.pdf  \
+    ./stat_factor/stat_factor.pdf stat_wells/stat_wells.pdf \
     stat_hadoop_patent/patent.pdf stat_hadoop_kmeans/kmeans.pdf \
     stat_hadoop_logreg/stat_hadoop_logreg.pdf \
     stat_hadoop_AtA_qr/stat_AtA_qr.pdf \
     stat_hadoop_rand_proj/stat_hadoop_rand_proj.pdf \
     stat_appendix/stat_normtable.pdf \
     output ~/Dropbox/Public/skfiles/stat.pdf
-    """)
+    """
+    print cmd
+    os.system(cmd)
+
     exit()
     
 if sys.argv[1] == 'all':
