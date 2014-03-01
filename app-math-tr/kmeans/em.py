7e7ab4f3bd02ec8e6f721613a57e2d5e2c574195
burak <burak@burak-Aspire-V5-122P.(none)>
2014-03-01 20:53:30 +0100

    -

--- app-math-tr/kmeans/em.py
+++ app-math-tr/kmeans/em.py
@@ -1,9 +1,8 @@
 # Multivariate gaussian, contours
 #
-import numpy as np, math
+import numpy as np, math, itertools
 import numpy.linalg as linalg
 import matplotlib.pylab as plt
-data = np.loadtxt('biometric_data_simple.txt',delimiter=',')
 
 def norm_pdf(x, mu, sigma):
   size = len(x)
@@ -18,39 +17,34 @@ def norm_pdf(x, mu, sigma):
   else:
      raise NameError("The dimensions of the input don't match")
 
+def plot(data, mus, sigmas):
+  plt.scatter (data[:,1],data[:,2])
+  plt.hold(True)
+  plt.xlim(55,80)
+  plt.ylim(80,280)
 
-plt.scatter (data[:,1],data[:,2])
-plt.hold(True)
-plt.xlim(55,80)
-plt.ylim(80,280)
+  x = np.arange(55., 80., 2)
+  y = np.arange(80., 280., 2)
+  X, Y = np.meshgrid(x, y)
 
-x = np.arange(55., 80., 2)
-y = np.arange(80., 280., 2)
-X, Y = np.meshgrid(x, y)
+  Z = np.zeros(X.shape)
+  nx, ny = X.shape
+  for mu,sigma in itertools.izip(mus,sigmas):
+    for i in xrange(nx):
+        for j in xrange(ny):
+            Z[i,j] = norm_pdf(np.array([X[i,j], Y[i,j]]),mu,sigma)
+    levels = np.linspace(Z.min(), Z.max(), 4)
+    plt.contour(X, Y, Z, colors='k', levels=levels)
+    plt.hold(True)
+  plt.show()
 
-Z = np.zeros(X.shape)
-nx, ny = X.shape
-mu1 = np.array([  72.,  193.217])
-sigma1 = np.matrix([[  7.84711,    10],[ 10,  1339.7028]])
-for i in xrange(nx):
-    for j in xrange(ny):
-        Z[i,j] = norm_pdf(np.array([X[i,j], Y[i,j]]),mu1,sigma1)
-        
-        
-levels = np.linspace(Z.min(), Z.max(), 4)
+mu1 = np.array([  65.,  200])
+sigma1 = np.matrix([[  7.,    10],[ 10,  1000]])
+mu2 = np.array([  72.,  200  ])
+sigma2 = np.matrix([[  7,    10],[ 10,  1000]])
+mus = []; mus.append(mu1); mus.append(mu2)
+sigmas = []; sigmas.append(sigma1); sigmas.append(sigma2)
 
-plt.contour(X, Y, Z, colors='k', levels=levels)
-plt.hold(True)
+data = np.loadtxt('biometric_data_simple.txt',delimiter=',')
+plot(data, mus, sigmas)
 
-Z = np.zeros(X.shape)
-nx, ny = X.shape
-mu2 = np.array([  66.,  135.308125  ])
-sigma2 = np.matrix([[  14.28189396,   10],[  10,  403.09566456]])
-for i in xrange(nx):
-    for j in xrange(ny):
-        Z[i,j] = norm_pdf(np.array([X[i,j], Y[i,j]]),mu2,sigma2)
-        
-levels = np.linspace(Z.min(), Z.max(), 4)
-
-plt.contour(X, Y, Z, colors='k', levels=levels)
-plt.show()
