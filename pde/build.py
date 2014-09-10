3a906431840ebba1fbc5b09e9855213a63d7727c
burak <burak@burak-Aspire-V5-122P.(none)>
2014-09-10 11:08:31 +0200

    -

--- pde/build.py
+++ pde/build.py
@@ -2,8 +2,8 @@ import os, sys, glob
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
     os.system("pdftk pde*/*.pdf \
-    heat/heat.pdf heat-deriv/heat-deriv.pdf level/level.pdf lk/lk.pdf curvature/curvature.pdf \
-    pde-wave-deriv/pde_01.pdf \
+    heat/heat.pdf heat-deriv/heat-deriv.pdf level/level.pdf \
+    lk/lk.pdf curvature/curvature.pdf wave-deriv/pde_01.pdf \
     output ~/Dropbox/Public/skfiles/pde.pdf"
 )
     exit()
