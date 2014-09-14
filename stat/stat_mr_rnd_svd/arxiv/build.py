4c78cfd2b4623018009961ea5434775bfc111d02
burak <burak@burak-Aspire-V5-122P.(none)>
2014-09-15 01:36:05 +0200

    -

--- stat/stat_mr_rnd_svd/arxiv/build.py
+++ stat/stat_mr_rnd_svd/arxiv/build.py
@@ -1,7 +1,7 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex mr_svd.tex")
-    os.system("evince mr_svd.pdf")
+    os.system("pdflatex mr_svd_2.tex")
+    os.system("evince mr_svd_2.pdf")
     exit()
 
