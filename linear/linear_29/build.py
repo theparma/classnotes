26147c597e69c205f81282715b3f834a9ced4539
burak <burak@burak-Aspire-V5-122P.(none)>
2014-09-18 10:33:01 +0200

    -

--- linear/linear_29/build.py
+++ linear/linear_29/build.py
@@ -1,7 +1,7 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex linear_*.tex")
+    os.system("pdflatex -shell-escape linear_*.tex")
     os.system("evince linear_*.pdf")
     exit()
     
