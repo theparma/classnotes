0e6e322886fa4af6049fa677c3f049cff1396412
burak <burak@burak-Aspire-V5-122P.(none)>
2014-10-15 16:40:27 +0200

    -

--- app-math-tr/mstseg/felz.py
+++ app-math-tr/mstseg/felz.py
@@ -1,8 +1,18 @@
+import scipy.sparse as sps
+import scipy.io as io
+import itertools
+
 def threshold(size, c): return c / size 
 
-def felzenswalb(E, t):
-    print list(sorted(E))
-    C = {u[1]:u[1] for u in E}
-    print C
-    ts = {x:threshold(1,t) for x in C}
-    print ts
+class Felzenswalb:
+    def __init__(self, t):
+        self.t_ = t
+
+    def fit(self, X):        
+        E = [(v,i,j) for i,j,v in itertools.izip(X.row, X.col, X.data)]
+        print list(sorted(E))
+        C = {u[1]:u[1] for u in E}
+        print C
+        ts = {x:threshold(1,self.t_) for x in C}
+        print ts
+    
