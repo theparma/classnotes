24ffaadcd61d043caf089562b4d28da515a0ccde
burak <burak@burak-Aspire-V5-122P.(none)>
2014-10-15 17:18:15 +0200

    -

--- app-math-tr/mstseg/felz.py
+++ app-math-tr/mstseg/felz.py
@@ -10,9 +10,11 @@ class Felzenswalb:
 
     def fit(self, X):        
         E = [(v,i,j) for i,j,v in itertools.izip(X.row, X.col, X.data)]
-        print list(sorted(E))
+        E = list(sorted(E))
         C = {u[1]:u[1] for u in E}
         print C
         ts = {x:threshold(1,self.t_) for x in C}
         print ts
+        for (v,i,j) in E:
+            print v,i,j
     
