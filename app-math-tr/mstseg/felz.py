958ca5831ac499b8469db8e216ae25a2c92d222d
burak <burak@burak-Aspire-V5-122P.(none)>
2014-10-15 15:56:13 +0200

    -

--- app-math-tr/mstseg/felz.py
+++ app-math-tr/mstseg/felz.py
@@ -1,3 +1,8 @@
-def felzenswalb(E):
+def threshold(size, c): return c / size 
+
+def felzenswalb(E, t):
     print list(sorted(E))
-    
+    C = {u[1]:u[1] for u in E}
+    print C
+    ts = {x:threshold(1,t) for x in C}
+    print ts
