412937d2ef523c1fb1ac4c904fc123c76a6b2e9c
burak <burak@burak-Aspire-V5-122P.(none)>
2014-10-16 11:58:38 +0200

    -

--- app-math-tr/mstseg/felz.py
+++ app-math-tr/mstseg/felz.py
@@ -4,17 +4,46 @@ import itertools
 
 def threshold(size, c): return c / size 
 
-class Felzenswalb:
-    def __init__(self, t):
-        self.t_ = t
+def find(C, u):
+    if C[u] != u:
+        C[u] = find(C, C[u])                    # Path compression
+    return C[u]
 
-    def fit(self, X):        
-        E = [(v,i,j) for i,j,v in itertools.izip(X.row, X.col, X.data)]
-        E = list(sorted(E))
-        C = {u[1]:u[1] for u in E}
-        print C
-        ts = {x:threshold(1,self.t_) for x in C}
-        print ts
-        for (v,i,j) in E:
-            print v,i,j
-    
+def union(C, R, u, v):
+    u, v = find(C, u), find(C, v)
+    if R[u] > R[v]:                             # Union by rank
+        C[v] = u
+    else:
+        C[u] = v
+    if R[u] == R[v]:                            # A tie: Move v up a level
+        R[v] += 1
+
+class Felzenswalb:
+    def __init__(self, threshold):
+        self.threshold_ = threshold
+
+    def fit(self, X):
+        print X.shape
+        G = {}
+        for i in range(X.shape[0]): G[i] = {}
+        for u,v,w in itertools.izip(X.row, X.col, X.data): G[u][v] = w
+        E = [(G[u][v],u,v) for u in G for v in G[u]]
+        T = set()
+        C, R = {u:u for u in G}, {u:0 for u in G}   # Comp. reps and ranks
+        
+        ts = {x:threshold(1,self.threshold_) for x in C}
+        
+        for _, u, v in sorted(E):
+            print u, v
+            if find(C, u) != find(C, v):
+                T.add((u, v))
+                print 'C1',C
+                union(C, R, u, v)
+                print 'C2',C
+                
+        #for (v,i,j) in E: print v,i,j
+
+        print T
+                
+        return T
+        
