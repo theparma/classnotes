f76d9774b772b71b76466c07a185883fab32f761
burak <burak@burak-Aspire-V5-122P.(none)>
2014-10-27 12:56:08 +0100

    -

--- stat/build.py
+++ stat/build.py
@@ -3,16 +3,15 @@ import os, sys, glob
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
     cmd = """
     pdftk \
-    ./stat_00/stat_00.pdf \
-    ./stat_intro/intro.pdf \
+    ./stat_00/stat_00.pdf ./stat_intro/intro.pdf \
     ./stat_summary/stat_summary.pdf \
     ./stat_tests/stat_tests.pdf ./stat_sample_size/stat_sample_size.pdf \
     ./stat_sampling_dist/stat_sampling_dist.pdf \
-    ./stat_mcmc/stat_mcmc.pdf \
-    ./stat_coal/stat_coal.pdf ./stat_mixbern/mixbern.pdf \
+    ./stat_mcmc/stat_mcmc.pdf ./stat_coal/stat_coal.pdf \
+    ./stat_mixbern/mixbern.pdf \
     ./stat_gauss_fusion/stat_gauss_fusion.pdf \
-    ./stat_pandas_ratings/ratings.pdf  \
-    ./stat_factor/stat_factor.pdf stat_wells/stat_wells.pdf \
+    ./stat_pandas_ratings/ratings.pdf ./stat_factor/stat_factor.pdf \
+    ./stat_wells/stat_wells.pdf ./stat_gmm/stat_gmm.pdf \
     ./stat_regular/regular.pdf stat_worldcup/stat_worldcup.pdf \
     ./stat_hadoop_patent/patent.pdf stat_hadoop_kmeans/kmeans.pdf \
     ./stat_hadoop_logreg/stat_hadoop_logreg.pdf \
