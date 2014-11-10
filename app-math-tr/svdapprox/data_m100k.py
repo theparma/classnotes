43d96d2239e27ae15a4849f236b1f0ab2cb09803
burak <burak@burak-Aspire-V5-122P.(none)>
2014-11-10 23:27:15 +0100

    -

--- app-math-tr/svdapprox/data_m100k.py
+++ app-math-tr/svdapprox/data_m100k.py
@@ -3,7 +3,7 @@ import os, numpy as np
 from scipy import sparse
 
 file_path = '%s/Downloads/ml-100k/u1.base' % os.environ['HOME']
-data = np.array([[int(tok) for tok in line.split('\t')[:3]]                 for line in open(file_path)])
+data = np.array([[int(tok) for tok in line.split('\t')[:3]] for line in open(file_path)])
 ij = data[:, :2]
 ij -= 1
 values = data[:, 2]
@@ -11,7 +11,7 @@ A = sparse.csc_matrix((values, ij.T)).astype(float)
 mmwrite('%s/Downloads/A_m100k_train'  % os.environ['HOME'], A)
 
 file_path = '%s/Downloads/ml-100k/u1.test' % os.environ['HOME']
-data = np.array([[int(tok) for tok in line.split('\t')[:3]]                 for line in open(file_path)])
+data = np.array([[int(tok) for tok in line.split('\t')[:3]]  for line in open(file_path)])
 ij = data[:, :2]
 ij -= 1
 values = data[:, 2]
