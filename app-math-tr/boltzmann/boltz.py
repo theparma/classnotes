1d459f04d567e79bcdfda6b723dcbf5b4057c22b
burak <burak@burak-Aspire-V5-122P.(none)>
2015-01-09 16:40:54 +0100

    -

--- app-math-tr/boltzmann/boltz.py
+++ app-math-tr/boltzmann/boltz.py
@@ -1,13 +1,15 @@
 class Boltzmann:
 
-    def __init__(self,n_iter=100,eta=0.1):
+    def __init__(self,n_iter=100,eta=0.1,sample_size=100,init_sample_size=10):
         self.n_iter = n_iter
         self.eta = eta
+        self.sample_size = sample_size
+        self.init_sample_size = init_sample_size
     
-    def sigmoid(u):
+    def sigmoid(self, u):
         return 1./(1.+np.exp(-u));
 
-    def draw(Sin,T):
+    def draw(self, Sin,T):
         """
         draw - perform single Gibbs sweep to draw a sample from distribution
         """
@@ -17,27 +19,28 @@ class Boltzmann:
         rand = np.random.rand(N,1)
         for i in xrange(N):
             h=np.dot(T[i,:],S)
-            S[i]=rand[i]<sigmoid(h);
+            S[i]=rand[i]<self.sigmoid(h);
 
         return S
 
-    def sample(T, n, num_init_samples):
+    def sample(self, T):
         N=T.shape[0]
-        print 'N',N
-        s=np.random.rand(N)<sigmoid(0)
-        for k in xrange(num_init_samples):
-            s=draw(s,T)
-        S=np.zeros((N,n))
+        s=np.random.rand(N)<self.sigmoid(0)
+        for k in xrange(init_sample_size):
+            s=self.draw(s,T)
+        S=np.zeros((N,sample_size))
         S[:,0]=s
-        for i in xrange(1,n):
-            S[:,i]=draw(S[:,i-1],T)
+        for i in xrange(1,sample_size):
+            S[:,i]=self.draw(S[:,i-1],T)
         return S
 
     def fit(self, X):
         R_data=np.dot(X,X.T)/A.shape[1];
-        print R_data
+        W_init = np.random.uniform(size=R_data.shape)
+        W = W_init.copy()
         for i in range(self.n_iter):
-            pass
+            print 'Iteration', i
+            
 
         return A
 
@@ -47,9 +50,9 @@ W = np.array([[0.2,0.3,0.5],
               [-0.2,0.1,-0.44],
               [0.2,-0.9,0.5]])
               
-#res = sample(W,20,5)
-#print res.shape
-#print res
+clf = Boltzmann()
+#clf.sample(W,20,5)
+#exit()
 
 A = np.array([\
 [0.,1.,1.,0],
@@ -59,6 +62,5 @@ A = np.array([\
 [0, 0, 1.,0]
 ])
 
-clf = Boltzmann()
 print clf.fit(A)
 
