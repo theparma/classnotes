8c07a44807a77a9a5e28b8452f39d92e3dafd750
burak <burak@burak-Aspire-V5-122P.(none)>
2015-01-09 16:47:34 +0100

    -

--- app-math-tr/boltzmann/boltz.py
+++ app-math-tr/boltzmann/boltz.py
@@ -15,44 +15,39 @@ class Boltzmann:
         """
         N=Sin.shape[0]
         S=Sin.copy()
-        print S
         rand = np.random.rand(N,1)
         for i in xrange(N):
             h=np.dot(T[i,:],S)
             S[i]=rand[i]<self.sigmoid(h);
-
         return S
 
     def sample(self, T):
         N=T.shape[0]
         s=np.random.rand(N)<self.sigmoid(0)
-        for k in xrange(init_sample_size):
+        for k in xrange(self.init_sample_size):
             s=self.draw(s,T)
-        S=np.zeros((N,sample_size))
+        S=np.zeros((N,self.sample_size))
         S[:,0]=s
-        for i in xrange(1,sample_size):
+        for i in xrange(1,self.sample_size):
             S[:,i]=self.draw(S[:,i-1],T)
-        return S
+        return S.T
 
     def fit(self, X):
-        R_data=np.dot(X,X.T)/A.shape[1];
-        W_init = np.random.uniform(size=R_data.shape)
+        W_data=np.dot(X.T,X)/X.shape[1];
+        print W_data.shape
+        W_init = np.random.uniform(size=W_data.shape)
         W = W_init.copy()
         for i in range(self.n_iter):
             print 'Iteration', i
-            
+            S = self.sample(W)
+            W_guess=np.dot(S.T,S)/S.shape[1];
+            W += self.eta * (W_data - W_guess)
 
-        return A
+        return W
 
 import numpy as np
 
-W = np.array([[0.2,0.3,0.5],
-              [-0.2,0.1,-0.44],
-              [0.2,-0.9,0.5]])
-              
 clf = Boltzmann()
-#clf.sample(W,20,5)
-#exit()
 
 A = np.array([\
 [0.,1.,1.,0],
