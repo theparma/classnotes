312394fec6c5dc0cfdcd31efa78c1229e6b58e1a
burak <burak@burak-Aspire-V5-122P.(none)>
2015-01-12 12:17:45 +0100

    -

--- app-math-tr/boltzmann/boltz.py
+++ app-math-tr/boltzmann/boltz.py
@@ -1,6 +1,6 @@
 class Boltzmann:
 
-    def __init__(self,n_iter=100,eta=0.01,sample_size=100,init_sample_size=10):
+    def __init__(self,n_iter=30,eta=0.1,sample_size=100,init_sample_size=10):
         self.n_iter = n_iter
         self.eta = eta
         self.sample_size = sample_size
@@ -18,12 +18,12 @@ class Boltzmann:
         rand = np.random.rand(N,1)
         for i in xrange(N):
             h=np.dot(T[i,:],S)
-            S[i]=2*rand[i]<self.sigmoid(h)-1;
+            S[i]=rand[i]<self.sigmoid(h);
         return S
 
     def sample(self, T):
         N=T.shape[0]
-        s=2*np.random.rand(N)<self.sigmoid(0)-1
+        s=np.random.rand(N)<self.sigmoid(0)
         for k in xrange(self.init_sample_size):
             s=self.draw(s,T)
         S=np.zeros((N,self.sample_size))
@@ -32,34 +32,40 @@ class Boltzmann:
             S[:,i]=self.draw(S[:,i-1],T)
         return S.T
 
+    def normc(self, X):
+        """
+        Return the normalization constant
+        """
+        S = self.sample(self.W)
+        print S
+        return 0
+
+    
     def fit(self, X):
+        W=np.zeros((X.shape[1],X.shape[1]))
         W_data=np.dot(X.T,X)/X.shape[1];
-        print W_data.shape
-        W_init = np.random.uniform(size=W_data.shape)
-        W_init = np.tril(W_init) + np.tril(W_init, -1).T
-        np.fill_diagonal(W_init, 0)
-        print W_init
-        W = W_init.copy()
+        print W_data
         for i in range(self.n_iter):
             print 'Iteration', i
             S = self.sample(W)
+            print S
             W_guess=np.dot(S.T,S)/S.shape[1];
             W += self.eta * (W_data - W_guess)
             np.fill_diagonal(W, 0)
-
-        return W
+        self.W = W
+        self.C = self.normc(X)
 
 import numpy as np
 
 clf = Boltzmann()
 
 A = np.array([\
-[-1.,1.,1.,-1],
-[1.,1.,-1,-1],
-[1.,1.,1.,-1],
-[-1, 1.,1.,1.],
-[1, -1, 1.,-1]
+[0.,1.,1.,0],
+[1.,1.,0,0],
+[1.,1.,1.,0],
+[0, 1.,1.,1.],
+[1, 0, 1.,0]
 ])
 
-print clf.fit(A)
-
+clf.fit(A)
+print clf.W
